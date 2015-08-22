be9c6f5f652db299c030a79d0f9d9cf9783ebac1
Nathaniel <nathaniel.arellano@gmail.com>
2015-08-22 16:00:56 -0700

    CSS
    Made look pretty.

--- backpack/app.js
+++ backpack/app.js
@@ -7,7 +7,7 @@ function Backpack() {
 	
 	}; */
 	this.self = this;	//black magic
-	this.backpack = ko.observableArray([]);
+	this.backpack = ko.observableArray([new Item()]);
 	this.blankItem = ko.observable(new Item());
 	
 	this.addItem = function() {
@@ -28,8 +28,8 @@ function Backpack() {
 };
 
 function Item() {
-	this.itemName = ko.observable('');
-	this.itemDesc = ko.observable('');
-	this.itemQty = ko.observable('');
-	this.itemWeight = ko.observable('');
+	this.itemName = ko.observable('Holy Hand Grenade');
+	this.itemDesc = ko.observable('First thou pullest the Holy Pin. Then thou must count to three. Three shall be the number of the counting and the number of the counting shall be three. Four shalt thou not count, neither shalt thou count two, excepting that thou then proceedeth to three. Five is right out. Once the number three, being the number of the counting, be reached, then lobbest thou the Holy Hand Grenade in the direction of thine foe, who, being naughty in my sight, shall snuff it.');
+	this.itemQty = ko.observable('x' + '5');
+	this.itemWeight = ko.observable('0.5' + 'lbs');
 };
