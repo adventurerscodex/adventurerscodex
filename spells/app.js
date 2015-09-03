bb3e51433ed0c0bbf4566886db655bf60e6071e4
Jim <krooskos@gmail.com>
2015-09-02 20:59:55 -0700

    Add persistence to spellbook array

--- spells/app.js
+++ spells/app.js
@@ -1,29 +1,41 @@
+"use strict";
+
 function Spellbook() {
     var self = this;
-    self.spellbook = ko.observableArray([]);
-    self.blankSpell = ko.observable(new Spell());
+    self.blankSpell = ko.observable(new Spell('', '', '', '', function(){}));
+    self.spellbook = ko.observableArray([], {
+        persist: 'spells.spellbook',
+        mapping: function(values){
+            return new Spell(values.spellName, values.spellSchool,
+                values.spellLevel, values.spellDescription, function() {
+                self.spellbook.valueHasMutated();
+            });
+        }
+    });
 
     this.addSpell = function() {
         self.spellbook.push(self.blankSpell());
-        self.blankSpell(new Spell());
+        self.blankSpell(new Spell('', '', '', '', function() {
+                self.spellbook.valueHasMutated();
+            }));
     };
 
     self.removeSpell = function(spell) { self.spellbook.remove(spell) }
 
 };
 
-function Spell() {
+function Spell(name, school, level, desc, callback) {
     var self = this;
 
-    self.spellName = ko.observable('');
+    self.spellName = ko.observable(name);
     self.spellName.subscribe(callback);
 
-    self.spellSchool = ko.observable('');
+    self.spellSchool = ko.observable(school);
     self.spellSchool.subscribe(callback);
 
-    self.spellLevel = ko.observable('');
+    self.spellLevel = ko.observable(level);
     self.spellSchool.subscribe(callback);
 
-    self.spellDescription = ko.observable('');
+    self.spellDescription = ko.observable(desc);
     self.spellDescription.subscribe(callback);
 };
