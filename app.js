68957c74ba49bd33b1e0b338811c5220584d0e27
Jim <krooskos@gmail.com>
2015-09-16 22:07:38 -0700

    Add spellbook to app

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ function ViewModel() {
 	this.note = ko.observable(new Note());
     this.abilityScores = ko.observable(new abilityScores());
     this.spellSlots = ko.observable(new SpellSlots());
-    
+    this.spellbook = ko.observable(new Spellbook());
+
     this.fileContents = ko.observable();
     this.fileReader = new FileReader();
-        
+
     this.clear = function() {
     	this.user().clear();
     	this.note().clear();
@@ -15,7 +16,7 @@ function ViewModel() {
     	this.stats().clear();
     	this.spellSlots().clear();
     };
-    
+
     this.importValues = function() {
     	var values = this.fileReader.json;
     	try {
@@ -24,25 +25,27 @@ function ViewModel() {
 			this.note().importValues(values.note);
 			this.abilityScores().importValues(values.abilityScores);
 			this.spellSlots().importValues(values.spellSlots);
+            this.spellbook().importValues(values.spellbook);
 		} catch(err) {
 			//Add error handling.
 			console.log(err);
-		}   
+		}
     };
-    
+
     this.exportValues = function() {
     	var string = JSON.stringify({
     		user: this.user().exportValues(),
     		note: this.note().exportValues(),
     		stats: this.stats().exportValues(),
-    		abilityScores: this.abilityScores().exportValues(),    	
-    		spellSlots: this.spellSlots().exportValues(),    	
+    		abilityScores: this.abilityScores().exportValues(),
+    		spellSlots: this.spellSlots().exportValues(),
+            spellbook: this.spellbook().exportValues(),
     	});
     	var filename = this.user().characterName();
     	var blob = new Blob([string], {type: "application/json"});
 		saveAs(blob, filename);
     };
-    
+
     //File handling
     this.fileReader.onload = function() {
     	var l = 'data:application/json;base64,'.length
