aafb9ce2790f2b42147481bbfc962364c7909896
Jim <krooskos@gmail.com>
2015-09-16 19:39:42 -0700

    Add fields to observable array

--- spells/app.js
+++ spells/app.js
@@ -3,14 +3,16 @@
 function Spellbook() {
     var self = this;
     self.selecteditem = ko.observable();
-    self.blankSpell = ko.observable(new Spell('', '', '', '', '', '', '', function(){}));
+    self.blankSpell = ko.observable(new Spell('', '', '', '',
+                                              '', '', '', '', function(){}));
     self.spellbook = ko.observableArray([], {
         persist: 'spells.spellbook',
         mapping: function(values){
             return new Spell(values.spellName, values.spellSchool,
-                values.spellLevel, values.spellDescription, function() {
-                self.spellbook.valueHasMutated();
-            });
+                             values.spellLevel, values.spellDescription,
+                             values.spellCastingTime, values.spellRange,
+                             values.spellComponents, values.spellDuration,
+                             function() {self.spellbook.valueHasMutated();});
         }
     });
 
@@ -28,7 +30,9 @@ function Spellbook() {
     };
 };
 
-function Spell(name, school, level, desc, callback) {
+function Spell(name, school, level, desc,
+               time, range, components,
+               duration, callback) {
     var self = this;
 
     self.spellName = ko.observable(name);
