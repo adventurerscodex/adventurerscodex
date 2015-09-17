a402a6060c49e91ffc9c8581e0abfcd74845940c
Jim <krooskos@gmail.com>
2015-09-16 22:08:27 -0700

    Add import, clear and export to spell function

--- spells/app.js
+++ spells/app.js
@@ -64,4 +64,45 @@ function Spell(name, type, dmg, school, level, desc,
 
     self.spellDuration = ko.observable(duration);
     self.spellDuration.subscribe(callback);
+
+    this.clear = function() {
+        self.spellName('');
+        self.spellType('');
+        self.spellDmg('');
+        self.spellSchool('');
+        self.spellLevel('');
+        self.spellDescription('');
+        self.spellCastingTime('');
+        self.spellRange('');
+        self.spellComponents('');
+        self.spellDuration('');
+    };
+
+    this.importValues = function(values) {
+        self.spellName(values.spellName);
+        self.spellType(values.spellType);
+        self.spellDmg(values.spellDmg);
+        self.spellSchool(values.spellSchool);
+        self.spellLevel(values.spellLevel);
+        self.spellDescription(values.spellDescription);
+        self.spellCastingTime(values.spellCastingTime);
+        self.spellRange(values.spellRange);
+        self.spellComponents(values.spellComponents);
+        self.spellDuration(values.spellDuration);
+    };
+
+    this.exportValues = function() {
+        return {
+        spellName: self.spellName(),
+        spellType: self.spellType(),
+        spellDmg: self.spellDmg(),
+        spellSchool: self.spellSchool(),
+        spellLevel: self.spellLevel(),
+        spellDescription: self.spellDescription(),
+        spellCastingTime: self.spellCastingTime(),
+        spellRange: self.spellRange(),
+        spellComponents: self.spellComponents(),
+        spellDuration: self.spellDuration(),
+        }
+    };
 };
