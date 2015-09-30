25c3770679e1d609eb77ce18d15dc5be2823226b
Nathaniel <nathaniel.arellano@gmail.com>
2015-09-29 18:41:54 -0700

    Added tests, import/export and stuff

--- backpack/app.js
+++ backpack/app.js
@@ -20,7 +20,7 @@ function Backpack() {
 		self.backpack.push(self.blankItem());
 		self.blankItem(new Item('','','','', function() {self.backpack.valueHasMutated();}));
 	};
-	self.removeItem = function(item) {
+	this.removeItem = function(item) {
 		self.backpack.remove(item);
 	};
 	
@@ -30,8 +30,6 @@ function Backpack() {
 	self.clear = function() {
 		self.backpack([]);
 	};
-	//this.importValues = function(values) {
-	//};
 	
 	self.exportValues = function() {
 		var backpack = [];
@@ -43,8 +41,18 @@ function Backpack() {
 			backpack: backpack
 		}
 	};
-};
 
+	self.importValues = function(values) {
+		var newItems = []
+		for (var i in values.backpack) {
+			var item = values.backpack[i];
+			var newItem = new Item('', '', '', '', function(){});
+			newItem.importValues(item);
+			self.backpack.push(newItem);
+		}
+	};
+};
+//TODO: Add units and make numbers.
 function Item(name, desc, qty, weight, callback) {
 	var self = this;
 	self.itemName = ko.observable(name);
@@ -58,4 +66,27 @@ function Item(name, desc, qty, weight, callback) {
 
 	self.itemWeight = ko.observable(weight);
 	self.itemWeight.subscribe(callback);
+
+	this.clear = function() {
+		self.itemName('');
+		self.itemDesc('');
+		self.itemQty('');
+		self.itemWeight('');
+	};
+	
+	this.importValues = function(values) {
+		self.itemName(values.itemName);
+		self.itemDesc(values.itemDesc);
+		self.itemQty(values.itemQty);
+		self.itemWeight(values.itemWeight);
+	};
+	
+	this.exportValues = function() {
+		return {
+			itemName: self.itemName(),
+			itemDesc: self.itemDesc(),
+			itemQty: self.itemQty(),
+			itemWeight: self.itemWeight()
+		}
+	};
 };
