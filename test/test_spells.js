aa69b0200d3039275362f0d543b2489169e49d65
Jim Krooskos <krooskos@gmail.com>
2015-10-22 06:58:26 -0700

    Add test for removing spells

--- test/test_spells.js
+++ test/test_spells.js
@@ -10,4 +10,17 @@ describe('Spells', function(){
       book.spellbook().length.should.equal(1);
     });
   });
+
+  describe('Remove Item', function() {
+    it('should remove a item from the spellbook', function() {
+      var book = new Spellbook();
+      book.clear();
+      book.spellbook().length.should.equal(0);
+      book.addSpell();
+      book.spellbook().length.should.equal(1);
+      book.removeSpell(book.spellbook().pop());
+      book.spellbook().length.should.equal(0);
+    });
+  });
+
 });
