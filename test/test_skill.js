d7a368e8b0d1d4a6a52839fdb432d7d31b5b9528
Brian Schrader <brian@brianschrader.com>
2015-10-25 12:34:51 -0700

    Fix issue #150

--- test/test_skill.js
+++ test/test_skill.js
@@ -2,34 +2,34 @@
 
 describe('Skill', function() {
 	describe('Bonus Label', function() {
-		it('should yield the bonus value (signed).', function() {
+		it('should yield the modifier value (signed).', function() {
 			var s = new Skill();
 			s.name('Arcana');
-			s.bonus(4);
+			s.modifier(4);
 			s.proficiency(true);
 			
-			s.bonusLabel().should.equal('+4');
+			s.modifierLabel().should.equal('+4 <i><small>()</small></i>');
 			
 			var s = new Skill();
 			s.name('Arcana');
-			s.bonus(-4);
+			s.modifier(-4);
 			s.proficiency(true);
 
-			s.bonusLabel().should.equal('-4');		
+			s.modifierLabel().should.equal('-4 <i><small>()</small></i>');		
 		});
 	});
 	describe('Proficiency Label', function() {
 		it('should yield the proficiency value (or none).', function() {
 			var s = new Skill();
 			s.name('Arcana');
-			s.bonus(4);
+			s.modifier(4);
 			s.proficiency(true);
 
 			s.proficiencyLabel().should.equal('glyphicon glyphicon-ok');
 			
 			var s = new Skill();
 			s.name('Arcana');
-			s.bonus(-4);
+			s.modifier(-4);
 			s.proficiency(false);
 
 			s.proficiencyLabel().should.equal('');		
@@ -39,15 +39,15 @@ describe('Skill', function() {
 		it('should clear all values', function() {
 			var s = new Skill();
 			s.name('Arcana');
-			s.bonus(4);
+			s.modifier(4);
 			s.proficiency(true);
 
 			s.name().should.equal('Arcana');
-			s.bonus().should.equal(4);
+			s.modifier().should.equal(4);
 			s.proficiency().should.equal(true);
 			s.clear();
 			s.name().should.equal('');
-			s.bonus().should.equal(0);
+			s.modifier().should.equal(0);
 			s.proficiency().should.equal(false);
 		});
 	});
@@ -56,15 +56,15 @@ describe('Skill', function() {
 		it('should yield an object with all the info supplied.', function() {
 			var s = new Skill();
 			s.name('Arcana');
-			s.bonus(4);
+			s.modifier(4);
 			s.proficiency(true);
 
 			s.name().should.equal('Arcana');
-			s.bonus().should.equal(4);
+			s.modifier().should.equal(4);
 			s.proficiency().should.equal(true);
 			var e = s.exportValues();
 			e.name.should.equal(s.name());
-			e.bonus.should.equal(s.bonus());
+			e.modifier.should.equal(s.modifier());
 			e.proficiency.should.equal(s.proficiency());
 		});
 	});
@@ -72,10 +72,10 @@ describe('Skill', function() {
 	describe('Import', function() {
 		it('should import an object with all the info supplied.', function() {
 			var s = new Skill('', 0, false, function(){});
-			var e = { name: 'Arcana', bonus: 3, proficiency: true };
+			var e = { name: 'Arcana', modifier: 3, proficiency: true };
 			s.importValues(e);
 			e.name.should.equal(s.name());
-			e.bonus.should.equal(s.bonus());
+			e.modifier.should.equal(s.modifier());
 			e.proficiency.should.equal(s.proficiency());
 		});
 	});
