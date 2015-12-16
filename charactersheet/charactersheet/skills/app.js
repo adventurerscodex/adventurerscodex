f27aaf09fb735d634c40811d93ec6b9f91c243bc
Jim Krooskos <krooskos@gmail.com>
2015-12-16 06:51:39 -0800

    Associate Survival with Wis

--- charactersheet/charactersheet/skills/app.js
+++ charactersheet/charactersheet/skills/app.js
@@ -2,7 +2,7 @@
 
 function SkillsViewModel() {
     var self = this;
-    
+
     self.sorts = {
 	  'name asc': { field: 'name', direction: 'asc'},
 	  'name desc': { field: 'name', direction: 'desc'},
@@ -11,7 +11,7 @@ function SkillsViewModel() {
 	  'proficiency asc': { field: 'proficiency', direction: 'asc'},
 	  'proficiency desc': { field: 'proficiency', direction: 'desc'}
 	};
-	
+
 	self._defaultSkills = function() {
 		var skills = [
 			{ name: 'Acrobatics', abilityScore: 'Dex', proficency: false, modifier: 0 },
@@ -31,7 +31,7 @@ function SkillsViewModel() {
 			{ name: 'Religion', abilityScore: 'Int', proficency: false, modifier: 0 },
 			{ name: 'Sleight of Hand', abilityScore: 'Dex', proficency: false, modifier: 0 },
 			{ name: 'Stealth', abilityScore: 'Dex', proficency: false, modifier: 0 },
-			{ name: 'Survival', abilityScore: 'Dex', proficency: false, modifier: 0 },
+			{ name: 'Survival', abilityScore: 'Wis', proficency: false, modifier: 0 },
 		];
 		return skills.map(function(e, i, _) {
 			var skill = new Skill(self);
@@ -40,19 +40,19 @@ function SkillsViewModel() {
 			return skill;
 		});
 	};
-		    
+
     self.selecteditem = ko.observable();
     self.blankSkill = ko.observable(new Skill(self));
     self.skills = ko.observableArray([]);
     self.filter = ko.observable('');
     self.sort = ko.observable(self.sorts['name asc']);
-    
+
     self.init = function() {};
-    
+
     self.load = function() {
     	var skills = Skill.findAllBy(CharacterManager.activeCharacter().key());
-    
-    	if (skills.length === 0) {    	
+
+    	if (skills.length === 0) {
     		self.skills(self._defaultSkills());
     		self.skills().forEach(function(e, i, _) {
     			e.save();
@@ -73,46 +73,46 @@ function SkillsViewModel() {
 	    	})
 	    });
     };
-    
+
     self.unload = function() {
     	$.each(self.skills(), function(_, e) {
     		e.save();
     	});
     };
-    
+
 	/* UI Methods */
-	
+
 	/**
 	 * Filters and sorts the skills for presentation in a table.
 	 */
     self.filteredAndSortedSkills = ko.computed(function() {
     	var skills = self.skills();
-    	
+
     	if (self.filter() !== '') {
     		//skills = skills.filter(function(a) {});
-    	}	
+    	}
 
     	return skills.sort(function(a, b) {
     		var asc = self.sort().direction === 'asc' ? true : false;
     		var res = null;
-    		
+
     		var aprop = a[self.sort().field]();
     		var bprop = b[self.sort().field]();
-    		
+
     		if (self.sort().numeric) {
 				aprop = parseInt(a[self.sort().field]());
 				bprop = parseInt(b[self.sort().field]());
     		}
-    		
+
     		if (asc) {
 	    		res = aprop > bprop ? 1 : -1;
     		} else {
 	    		res = aprop < bprop ? 1 : -1;
     		}
     		return res;
-    	}); 	
+    	});
     });
-    
+
     /**
      * Determines whether a column should have an up/down/no arrow for sorting.
      */
@@ -138,7 +138,7 @@ function SkillsViewModel() {
 			sort = self.sorts[columnName+' desc'];
 		} else {
 			sort = self.sorts[columnName+' asc'];
-		}	
+		}
 		self.sort(sort);
 	};
 
@@ -151,15 +151,15 @@ function SkillsViewModel() {
         self.blankSkill(new Skill());
     };
 
-    self.removeSkill = function(skill) { 
+    self.removeSkill = function(skill) {
     	self.skills.remove(skill);
-    	skill.delete(); 
+    	skill.delete();
     };
 
     self.editSkill = function(skill) {
         self.selecteditem(skill);
     };
-    
+
     self.clear = function() {
     	self.skills().forEach(function(e, i, _) {
     		self.removeSkill(e);
