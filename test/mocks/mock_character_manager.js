88e133acdef4e3b68bd1cf73a23c61b375eadfd2
Brian Schrader <brian@brianschrader.com>
2016-02-12 13:40:32 -0800

    Adds tests for otherstats and converts some old tests

--- test/mocks/mock_character_manager.js
+++ test/mocks/mock_character_manager.js
@@ -1,5 +1,14 @@
-CharacterManager.activeCharacter = function() {
-    return {
-        key: function() { return '1234'; }
-    };
+"use strict";
+
+var MockCharacterManager = {};
+
+
+/**
+ * Returns a player character with the key '1234'.
+ */
+MockCharacterManager.activeCharacter = function() {
+    var c = new Character();
+    c.key('1234');
+    c.playerType(PlayerTypes.characterPlayerType);
+    return c;
 };
