cdf81a71428ffa211c151d8898855c2c69ef80f6
Brian Schrader <brian@brianschrader.com>
2016-03-07 17:23:54 -0800

    Fixes errors in items totalWeight

--- charactersheet/charactersheet/models/item.js
+++ charactersheet/charactersheet/models/item.js
@@ -14,7 +14,7 @@ function Item() {
 	self.itemIsEquippable = ko.observable(false);
 	self.itemBodyLocation = ko.observable('');
 	self.itemDesc = ko.observable('');
-	self.itemQty = ko.observable(0);
+	self.itemQty = ko.observable(1);
 	self.itemWeight = ko.observable(0);
 	self.itemCost = ko.observable(0);
 	self.itemCurrencyDenomination = ko.observable('');
