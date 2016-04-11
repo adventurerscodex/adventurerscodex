a79e92a89833cfd0800cb5462e3cc7cb0b083166
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2016-04-11 10:16:54 -0700

    Added various tests and had to change some models to be testable. (#511)

---
+++ test/models/test_character.js
@@ -0,0 +1,42 @@
+"use strict";
+
+describe('Character Model', function() {
+    //Clean up after each test.
+    afterEach(function() {
+        simple.restore();
+    });
+
+	describe('Save', function() {
+		it('should save the values.', function() {
+			var ft = new Character();
+			var saved = false;
+			ft.ps.save = function() { saved = true; };
+
+			saved.should.equal(false);
+			ft.save();
+			saved.should.equal(true);
+		});
+	});
+
+	describe('Export', function() {
+		it('should export the values.', function() {
+			var ft = new Character();
+			ft.isDefault(true);
+			ft.isDefault().should.equal(true);
+			var e = ft.exportValues();
+			ft.isDefault().should.equal(e.isDefault);
+		});
+	});
+
+  describe('Delete', function() {
+    it('should delete the objects.', function() {
+      var ft = new Character();
+      var deleted = false;
+      ft.ps.delete = function() { deleted = true; };
+
+      deleted.should.equal(false);
+      ft.delete();
+      deleted.should.equal(true);
+    });
+  });
+});
--- test/models/test_spell_stats.js
+++ test/models/test_spell_stats.js
@@ -10,4 +10,55 @@ describe('SpellStats Model', function() {
 			pc_stats.spellcastingAbility().should.equal('');
 		});
 	});
+
+	describe('Import', function() {
+		it('should import the values.', function() {
+			var ft = new SpellStats();
+			var e = {
+				spellcastingAbility: 'INT'
+			};
+			ft.spellcastingAbility().should.equal('');
+			ft.importValues(e);
+			ft.spellcastingAbility().should.equal(e.spellcastingAbility);
+		});
+	});
+
+	describe('Export', function() {
+		it('should export the values.', function() {
+			var ft = new SpellStats();
+			ft.spellcastingAbility('INT');
+			ft.spellcastingAbility().should.equal('INT');
+			var e = ft.exportValues();
+			ft.spellcastingAbility().should.equal(e.spellcastingAbility);
+		});
+	});
+
+	describe('Save', function() {
+		it('should save the values.', function() {
+			var ft = new SpellStats();
+			var saved = false;
+			ft.ps.save = function() { saved = true; };
+
+			saved.should.equal(false);
+			ft.save();
+			saved.should.equal(true);
+		});
+	});
+
+	describe('Find By', function() {
+		it('should find all of the values in the db.', function() {
+			var key = '1234';
+			simple.mock(PersistenceService, 'findAll').returnWith([new SpellStats()]);
+			var r = SpellStats.findBy(key);
+			r.length.should.equal(0);
+
+
+			simple.mock(PersistenceService, 'findAll').returnWith([new SpellStats()].map(function(e, i, _) {
+				e.characterId(key);
+				return e;
+			}));
+			var r = SpellStats.findBy(key);
+			r.length.should.equal(1);
+		});
+	});
 });
