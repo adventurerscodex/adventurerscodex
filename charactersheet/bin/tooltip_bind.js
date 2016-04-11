c30844e565f05b8f67afdd68c6a86e1e41152f11
Jim Krooskos <krooskos@gmail.com>
2016-04-11 07:24:21 -0700

    Add tooltip binding

---
+++ charactersheet/bin/tooltip_bind.js
@@ -0,0 +1,25 @@
++function(){
+
+  'use strict';
+
+  ko.bindingHandlers.tooltip = {
+    init: function(element, valueAccessor) {
+      var local = ko.utils.unwrapObservable(valueAccessor()),
+          options = {};
+
+      ko.utils.extend(options, ko.bindingHandlers.tooltip.options);
+      ko.utils.extend(options, local);
+
+      $(element).tooltip(options);
+
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+          $(element).tooltip("destroy");
+      });
+    },
+    options: {
+      placement: "top",
+      trigger: "hover"
+    }
+  };
+
+}();
