012daa8105838d18b79458e259863bc724933bf3
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2016-04-17 13:26:26 -0700

    Added hotkey functionality (#515)
    * Added function to change tabs using hotkeys
    
    * Added binding file.
    
    * Added new service to add hotkey functionality
    
    * Removed redundant code in root.js

--- charactersheet/charactersheet/viewmodels/root.js
+++ charactersheet/charactersheet/viewmodels/root.js
@@ -264,8 +264,19 @@ function RootViewModel() {
 		    self.connected(false);
 		});
 		Notifications.global.unload.add(self.unload);
+		HotkeysService.registerHotkey('1', self.activateProfileTab);
+		HotkeysService.registerHotkey('2', self.activateStatsTab);
+		HotkeysService.registerHotkey('3', self.activateSkillsTab);
+		HotkeysService.registerHotkey('4', self.activateSpellsTab);
+		HotkeysService.registerHotkey('5', self.activateEquipmentTab);
+		HotkeysService.registerHotkey('6', self.activateInventoryTab);
+		HotkeysService.registerHotkey('7', self.activateNotesTab);
+		HotkeysService.registerHotkey('8', function() {
+    		if(self.partyTabStatus() !== 'hidden'){
+      			self.activatePartyTab();
+    		}
+		});
 	};
-
 	/**
 	 * Signal all modules to load their data.
 	 */
