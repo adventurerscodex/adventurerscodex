9052bacb2c3b6d6997c986ae981cd05f2f5b7b26
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2016-06-28 16:50:59 -0700

    Tab tests (#711)
    * Changes stats layout
    
    * Redesigns more tabs: Stats, and reorders tabs.
    
    * Fixes test fixtures
    
    * Changes tables accroding to RFC 653
    
    * Changes stats layout
    
    * Redesigns more tabs: Stats, and reorders tabs.
    
    * Changes tables accroding to RFC 653
    
    * Adds sorting by proficient/prepared back with shorter icon header
    
    * Added tests for all tab view models

---
+++ test/viewmodels/test_skill_tab.js
@@ -0,0 +1,56 @@
+'use strict';
+
+describe('SkillsTabViewModel', function(){
+    //Clean up after each test.
+    afterEach(function() {
+        simple.restore();
+    });
+
+    describe('Init', function() {
+        it('should init the sub view models', function() {
+            var vm = new SkillsTabViewModel();
+            var notifySpy = simple.mock(ViewModelUtilities, 'initSubViewModels');
+
+            vm.init();
+
+            notifySpy.called.should.equal(true);
+        });
+    });
+
+    describe('Load', function() {
+        it('should load the sub view models', function() {
+            var vm = new SkillsTabViewModel();
+            simple.mock(ViewModelUtilities, 'loadSubViewModels').returnWith(true);
+            var notifySpy = simple.mock(ViewModelUtilities, 'loadSubViewModels');
+
+
+            vm.load();
+
+            notifySpy.called.should.equal(true);
+        });
+    });
+
+    describe('Unload', function() {
+        it('should unload the sub view models', function() {
+            var vm = new SkillsTabViewModel();
+            var notifySpy = simple.mock(ViewModelUtilities, 'unloadSubViewModels');
+
+            vm.unload();
+
+            notifySpy.called.should.equal(true);
+        });
+    });
+
+    describe('Clear', function() {
+        it('should clear the sub view models', function() {
+            var vm = new SkillsTabViewModel();
+            simple.mock(ViewModelUtilities, 'clearSubViewModels').returnWith(true);
+            var notifySpy = simple.mock(ViewModelUtilities, 'clearSubViewModels');
+
+            vm.clear();
+
+            notifySpy.called.should.equal(true);
+        });
+    });
+
+});
