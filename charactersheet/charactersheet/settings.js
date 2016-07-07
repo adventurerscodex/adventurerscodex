ccc805112b6bf58fb9e7716d0376937e6fc98cad
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2016-07-06 17:50:26 -0700

    rearranged treasure module

--- charactersheet/charactersheet/settings.js
+++ charactersheet/charactersheet/settings.js
@@ -11,25 +11,25 @@ var Settings = {
     /**
      * A config object for dropbox integration.
      */
-    // dropboxConfigOptions: {
-    //     // Required. Called when a user selects an item in the Chooser.
-    //     success: function(files) {
-    //         WizardViewModel.importRemoteFile(files);
-    //     },
-    //     // Optional. Called when the user closes the dialog without selecting a file
-    //     // and does not include any parameters.
-    //     cancel: function() {},
-    //     // Optional. "preview" (default) is a preview link to the document for sharing,
-    //     // "direct" is an expiring link to download the contents of the file. For more
-    //     // information about link types, see Link types below.
-    //     linkType: 'direct', // or "direct"
-    //     // Optional. A value of false (default) limits selection to a single file, while
-    //     // true enables multiple file selection.
-    //     multiselect: false, // or true
-    //     // Optional. This is a list of file extensions. If specified, the user will
-    //     // only be able to select files with these extensions. You may also specify
-    //     // file types, such as "video" or "images" in the list. For more information,
-    //     // see File types below. By default, all extensions are allowed.
-    //     extensions: ['.json']
-    // }
+    dropboxConfigOptions: {
+        // Required. Called when a user selects an item in the Chooser.
+        success: function(files) {
+            WizardViewModel.importRemoteFile(files);
+        },
+        // Optional. Called when the user closes the dialog without selecting a file
+        // and does not include any parameters.
+        cancel: function() {},
+        // Optional. "preview" (default) is a preview link to the document for sharing,
+        // "direct" is an expiring link to download the contents of the file. For more
+        // information about link types, see Link types below.
+        linkType: 'direct', // or "direct"
+        // Optional. A value of false (default) limits selection to a single file, while
+        // true enables multiple file selection.
+        multiselect: false, // or true
+        // Optional. This is a list of file extensions. If specified, the user will
+        // only be able to select files with these extensions. You may also specify
+        // file types, such as "video" or "images" in the list. For more information,
+        // see File types below. By default, all extensions are allowed.
+        extensions: ['.json']
+    }
 };
