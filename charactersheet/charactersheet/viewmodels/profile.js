0c0c8870e62a2e17181451215f4fa46d42f0e79c
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2016-08-21 13:19:35 -0700

    Fixed some tests.

--- charactersheet/charactersheet/viewmodels/profile.js
+++ charactersheet/charactersheet/viewmodels/profile.js
@@ -27,18 +27,18 @@ function ProfileViewModel() {
     self.load = function() {
         var profile = Profile.findBy(CharacterManager.activeCharacter().key())[0];
         if (profile) {
-          self.level(profile.level());
-          self.playerName(profile.playerName());
-          self.characterName(profile.characterName());
-          self.background(profile.background());
-          self.race(profile.race());
-          self.religion(profile.religion());
-          self.alignment(profile.alignment());
-          self.diety(profile.diety());
-          self.typeClass(profile.typeClass());
-          self.gender(profile.gender());
-          self.age(profile.age());
-          self.experience(profile.exp());
+            self.level(profile.level());
+            self.playerName(profile.playerName());
+            self.characterName(profile.characterName());
+            self.background(profile.background());
+            self.race(profile.race());
+            self.religion(profile.religion());
+            self.alignment(profile.alignment());
+            self.diety(profile.diety());
+            self.typeClass(profile.typeClass());
+            self.gender(profile.gender());
+            self.age(profile.age());
+            self.experience(profile.exp());
         }
 
         //Subscriptions
@@ -84,17 +84,17 @@ function ProfileViewModel() {
     //Public Methods
 
     self.clear = function() {
-      self.characterName('');
-      self.background('');
-      self.playerName('');
-      self.race('');
-      self.religion('');
-      self.alignment('');
-      self.diety('');
-      self.typeClass('');
-      self.gender('');
-      self.age('');
-      self.level('');
-      self.experience('');
+        self.characterName('');
+        self.background('');
+        self.playerName('');
+        self.race('');
+        self.religion('');
+        self.alignment('');
+        self.diety('');
+        self.typeClass('');
+        self.gender('');
+        self.age('');
+        self.level('');
+        self.experience('');
     };
 }
