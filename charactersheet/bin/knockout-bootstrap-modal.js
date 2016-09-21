e954a301de15c7d4aa0adac68544d502fd49b3fe
Brian Schrader <brian@brianschrader.com>
2016-09-21 16:29:30 -0700

    Adds KO Binding For Modals (#799)
    * Adds Modal Binding
    
    - Identical to Well Binding: provides an open/closed value binding
    as well as an optional callback for when the animation has finished.
    
    - This commit also implements the first use of the callback to focus
    the first element of the Skills modal once the animation has finished.
    
    - Known issues: Currently the first time the modal is opened programatically
    the animation doesn't happen correctly. Will look further into this.
    
    * Fixes issue noted in last commit.
    
    - Where modal transitions would fail on first toggle.
    
    * Adds Programatic Modals to Ability Scores
    
    - Cleans up Skills Modals.

---
+++ charactersheet/bin/knockout-bootstrap-modal.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Knockout Bootstrap Modal Binding
+ * author: Brian Schrader
+ *
+ * Open and close a bootstrap modal using an observable.
+ * This binding also has an optional field for a callback
+ * once the animation has completed.
+ *
+ * Note: The callback is called when the modal is both opened and closed.
+ *
+ * Usage:
+ * <div data-bind="modal: { open: myObservable, callback: myFunction }"></div>
+ */
+ko.bindingHandlers.modal = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+        var value = valueAccessor();
+        var openOrClosed = ko.utils.unwrapObservable(value.open);
+        var callback = ko.utils.unwrapObservable(value.callback);
+
+        ko.bindingHandlers.modal.toggle(openOrClosed, element);
+
+        if (callback) {
+            // Register callbacks.
+            $(element).on('hidden.bs.modal', callback);
+            $(element).on('shown.bs.modal', callback);
+        }
+    },
+
+    update: function(element, valueAccessor) {
+        var value = valueAccessor();
+        var openOrClosed = ko.utils.unwrapObservable(value.open);
+        ko.bindingHandlers.modal.toggle(openOrClosed, element);
+    },
+
+    toggle: function(openOrClosed, element) {
+        var action = openOrClosed ? 'show' : 'hide';
+        $(element).modal(action);
+    }
+};
