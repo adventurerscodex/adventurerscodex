fd8be919fb7891384113c97dfec3329068fb84b1
Jim Krooskos <krooskos@gmail.com>
2016-10-02 15:32:23 -0700

    Add migration

---
+++ charactersheet/charactersheet/migrations/110_3_stats.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+* Sets proficiency to 0. This attribute is now considered a modifer and should
+* be zero after the migration
+*/
+var migration_110_3_stats = {
+    name: 'Stats Proficiency Values Migration',
+    version: '1.1.0',
+    migration: function() {
+        var stats = PersistenceService.findAllObjs('OtherStats');
+        for (var i in stats) {
+            var id = stats[i].id;
+            var stat = stats[i].data;
+
+            stat.proficiency = 0
+
+            PersistenceService.saveObj('OtherStats', id, stat);
+        }
+    }
+};
