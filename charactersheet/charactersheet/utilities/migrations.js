61010fe8de0a2653bb7bd0f67ed279130245a43a
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2016-10-10 14:42:17 -0700

    Moved migrations to its own file.

---
+++ charactersheet/charactersheet/utilities/migrations.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Migrations = {
+    // List all migrations that should be applied
+    scripts : [
+        migration_110_1_skills,
+        migration_110_2_spells,
+        migration_110_3_stats
+    ]
+};
