ee401ab8a0349c2c44ca534f4322d1fbc7690a22
Brian Schrader <brian@brianschrader.com>
2016-10-13 14:25:35 -0700

    Adds new player types layout

--- charactersheet/charactersheet/models/player_types.js
+++ charactersheet/charactersheet/models/player_types.js
@@ -3,7 +3,14 @@
 var PlayerTypes = {
     characterPlayerType: {
         key: 'character',
-        visibleTabs: ['profile', 'stats', 'skills', 'spells', 'equipment', 'inventory', 'notes', 'settings', 'party'],
+        visibleTabs: [
+            'profile', 'stats', 'skills', 'spells', 'equipment',
+            'inventory', 'notes', 'settings', 'party'],
+        tabViewModels: [
+            ProfileTabViewModel, StatsTabViewModel, SkillsTabViewModel,
+            SpellsTabViewModel, EquipmentTabViewModel, InventoryTabViewModel,
+            NotesTabViewModel
+        ],
         defaultTab: 'stats'
     }
 };
