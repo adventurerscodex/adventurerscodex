bb4543940da8becf11b4702021c5aedaf0694517
Brian Schrader <brian@brianschrader.com>
2016-10-14 16:22:29 -0700

    Adds wizard for DM campaigns, and adds switching to DMs.
    - The wizard can now be used to create DM characters.
        - The DMs, when loaded will have a test page of content. This page can be
          used to add additional DM modules.
    - Character switching is now done in a way that unfortunately makes switching
      even slower (for now, there are ways to fix this). A character's type
    determines which root view model to load and then inits and loads it.
        - Because of the fact that multiple player view models are now used
          (instead of the 1 root vm) the importance of the `init` method on child
    view models is greatly reduced, and should be considered for deprication.
    - Currently the player info VMs do not load for DMs but this is expected.
      There is an exception in the index.html that refuses to load those VMs
    except if the player is a DM.
    - Modules form this point forward can be added to the DM root view model. They
      should work fine. As for the player Info, this will need to be fixed soon.

---
+++ charactersheet/charactersheet/viewmodels/common/wizard_steps/wizard_player_type_step.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function WizardPlayerTypeStepViewModel() {
+    var self = this;
+
+    self.TEMPLATE_FILE = 'wizard_player_type_step.tmpl';
+    self.IDENTIFIER = 'WizardPlayerTypeStep';
+
+    self.REQUIRED_FIELDS = ['playerType'];
+
+    // View Model Methods
+
+    self.init = function() { };
+
+    self.load = function() {
+    };
+
+    self.unload = function() {};
+
+    // View Properties
+
+    self.playerType = ko.observable();
+
+    // Wizard Step Methods
+
+    /**
+     * Returns true if all required fields are filled.
+     */
+    self.ready = ko.pureComputed(function() {
+        var emptyFields = self.REQUIRED_FIELDS.filter(function(field, idx, _) {
+            return self[field]() ? !self[field]().trim() : true;
+        });
+        return emptyFields.length === 0;
+    });
+
+    /**
+     * Returns an object containing the current values for
+     * the fields in the form.
+     */
+    self.results = ko.pureComputed(function() {
+        return {
+            playerType: PlayerTypes[self.playerType()]
+        };
+    });
+}
