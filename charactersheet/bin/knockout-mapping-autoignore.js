f56152c4a3d6f63d6a479fd1f61ae53db60ab8d5
Brian Schrader <brian@brianschrader.com>
2016-10-17 18:39:48 -0700

    Adds new autoignore binding

---
+++ charactersheet/bin/knockout-mapping-autoignore.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * A Knockout Mapping addition that will automatically add any field not
+ * included in the `include` list to the ignore list.
+ * This eliminates the need to explicitly enter both includes and ignores.
+ *
+ * Usage:
+ *
+ *   var mapping = {
+ *       include: ['name', 'address', 'zip code']
+ *   });
+ */
+ko.mapping.autoignore = function(obj, config) {
+    var includes = config.include || [];
+    var keys = Object.keys(obj);
+
+    config.ignore = config.ignore || [];
+    config.ignore = config.ignore.concat(keys.filter(function(key, idx, _) {
+        return includes.indexOf(key) === -1;
+    }));
+    return config;
+};
