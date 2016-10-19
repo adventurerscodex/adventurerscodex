5a9b2b79d4f579d30bf978f306f290f5f7e095f5
Brian Schrader <brian@brianschrader.com>
2016-10-18 21:51:20 -0700

    Initial commit

---
+++ charactersheet/charactersheet/viewmodels/dm/encounter.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function EncounterViewModel() {
+    var self = this;
+
+    self.encounters = ko.observableArray();
+    self.selectedEncounter = ko.observable();
+
+    // Public Methods
+
+    self.init = function() {
+
+    };
+
+    self.load = function() {
+
+    };
+
+    self.unload = function() {
+
+    };
+
+    // TODO: Manage Encounter Methods
+}
---
+++ charactersheet/charactersheet/viewmodels/dm/encounter_tab.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function EncounterTabViewModel() {
+    var self = this;
+
+    self.encounterViewModel = ko.observable();
+
+    //Public Methods
+
+    /**
+     * Call Init on each sub-module.
+     */
+    self.init = function() {
+        ViewModelUtilities.initSubViewModels(self);
+    };
+
+    /**
+     * Signal all modules to load their data.
+     */
+    self.load = function() {
+        ViewModelUtilities.loadSubViewModels(self);
+    };
+
+    self.unload = function() {
+        ViewModelUtilities.unloadSubViewModels(self);
+    };
+}
