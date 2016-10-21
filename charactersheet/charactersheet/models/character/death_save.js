ac5ea251924d8efe9fcc09380f2939a162ecf511
Jim Krooskos <krooskos@gmail.com>
2016-10-21 14:53:13 -0700

    Add death save icon functionality

--- charactersheet/charactersheet/models/character/death_save.js
+++ charactersheet/charactersheet/models/character/death_save.js
@@ -10,6 +10,40 @@ function DeathSave() {
     self.deathSaveSuccess = ko.observable(false);
     self.deathSaveFailure = ko.observable(false);
 
+    self.deathSaveSuccessHandler = function() {
+        if(self.deathSaveSuccess() === true) {
+            self.deathSaveSuccess(false);
+        } else if (self.deathSaveSuccess() === false) {
+            self.deathSaveSuccess(true);
+        }
+    };
+
+    self.deathSaveFailureHandler = function() {
+        if(self.deathSaveFailure() === true) {
+            self.deathSaveFailure(false);
+        } else if (self.deathSaveFailure() === false) {
+            self.deathSaveFailure(true);
+        }
+    };
+
+    self.deathSaveSuccessIcon = ko.pureComputed(function() {
+        if (self.deathSaveSuccess() === true) {
+            var css = 'ds-success-full';
+        } else if (self.deathSaveSuccess() === false){
+            var css = 'ds-success-empty';
+        }
+        return css;
+    });
+
+    self.deathSaveFailureIcon = ko.pureComputed(function() {
+        if (self.deathSaveFailure() === true) {
+            var css = 'ds-failure-full';
+        } else if (self.deathSaveFailure() === false){
+            var css = 'ds-failure-empty';
+        }
+        return css;
+    });
+
     self.clear = function() {
         var values = new DeathSave().exportValues();
         ko.mapping.fromJS(values, self.mapping, self);
