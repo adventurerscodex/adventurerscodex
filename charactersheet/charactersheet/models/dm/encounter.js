a04f9818722e39a11c25a1800d3b6de5da8f2da5
Brian Schrader <brian@brianschrader.com>
2016-10-21 14:38:08 -0700

    Adds support for nested encounters.

--- charactersheet/charactersheet/models/dm/encounter.js
+++ charactersheet/charactersheet/models/dm/encounter.js
@@ -6,6 +6,10 @@
 function Encounter() {
     var self = this;
     self.ps = PersistenceService.register(Encounter, self);
+    self.mapping = {
+        include: ['characterId', 'encounterId', 'name', 'locale', 'notes',
+            'parent', 'children']
+    };
 
     // General Encounter Properties.
     self.characterId = ko.observable();
@@ -17,4 +21,36 @@ function Encounter() {
     // Related Encounter IDs
     self.parent = ko.observable();
     self.children = ko.observableArray([]);
+
+    // Public Methods
+
+    /**
+     * Returns the list of encounter objects corresponding to the child ids.
+     */
+    self.getChildren = function() {
+        return self.children().map(function(id, idx, _) {
+            return PersistenceService.findFirstBy(Encounter, 'encounterId', id);
+        });
+
+    };
+
+    self.save = function() {
+        self.ps.save();
+    };
+
+    self.clear = function() {
+        var values = new Encounter().exportValues();
+        var mapping = ko.mapping.autoignore(self, self.mapping);
+        ko.mapping.fromJS(values, mapping, self);
+    };
+
+    self.importValues = function(values) {
+        var mapping = ko.mapping.autoignore(self, self.mapping);
+        ko.mapping.fromJS(values, mapping, self);
+    };
+
+    self.exportValues = function() {
+        var mapping = ko.mapping.autoignore(self, self.mapping);
+        return ko.mapping.toJS(self, mapping);
+    };
 }
