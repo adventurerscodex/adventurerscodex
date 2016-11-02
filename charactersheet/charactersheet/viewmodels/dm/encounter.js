7ef0b43d6ba6d218c07f8cc67e42a0d16ca30441
Brian Schrader <brian@brianschrader.com>
2016-11-02 12:17:10 -0700

    Adds Encounter Detail View Model, and Fixes Switching
    - Encounters now generage an Encounter Id when created. This ID is used to
      compare for equality.
    - Encounter Detail View does nothing yet.
    - Fixes an issue where selected encounter events didn't propogate.
    - Selected Encounters are now styled.
    - Adds a basic encounter add modal. More to come.

--- charactersheet/charactersheet/viewmodels/dm/encounter.js
+++ charactersheet/charactersheet/viewmodels/dm/encounter.js
@@ -3,16 +3,21 @@
 function EncounterViewModel() {
     var self = this;
 
+    self.modalEncounter = ko.observable();
+    self.nameHasFocus = ko.observable(false);
     self.selectedEncounter = ko.observable();
     self.encounters = ko.observableArray();
     self._encounterDetailViewModel = ko.observable();
 
     self.init = function() {
-
+        self.selectedEncounter.subscribe(function() {
+            console.log('changed');
+        });
     };
 
     self.load = function() {
         self.encounters(self._getTopLevelEncounters());
+        self.selectedEncounter(self.encounters()[0]);
     };
 
     self.unload = function() {
@@ -21,23 +26,46 @@ function EncounterViewModel() {
 
     // UI Methods
 
+    /**
+     * Returns the current internal encounter detail view model, if the encounter
+     * changes then it deinitializes the old value and spins up a new
+     * EncounterDetailViewModel with the value of the current encounter.
+     */
     self.encounterDetailViewModel = ko.pureComputed(function() {
+        if (self._encounterDetailViewModel()) {
+            self._deinitializeDetailViewModel();
+        }
+
         var newEncounter = self.selectedEncounter();
-        self._deinitializeDetailViewModel();
         self._encounterDetailViewModel(new EncounterDetailViewModel(newEncounter));
         self._initializeDetailViewModel();
-        return self._encounterDetailViewModel();
+
+        return self._encounterDetailViewModel;
     });
+
+    // Modal Methods
+
+    self.openAddModal = function() {
+        self.modalEncounter(new Encounter());
+    };
+
+    self.modalFinishedOpening = function() {
+        self.nameHasFocus(true);
+    };
+
+    self.modalFinishedClosing = function() {
+        self.modalEncounter(null);
+    };
 
     self.addTopLevelEncounter = function() {
         var key = CharacterManager.activeCharacter().key();
-        var encounter = new Encounter();
+        var encounter = self.modalEncounter();
         encounter.characterId(key);
-        encounter.name('A new encounter');
         encounter.save();
 
         // Reload Encounters
         self.encounters(self._getTopLevelEncounters());
+        self.selectedEncounter(encounter);
     };
 
     // TODO: Manage Encounter Methods
