98c2b1f051b9be9c7fffebdaef6aa07bc24970cd
Brian Schrader <brian@brianschrader.com>
2016-11-03 17:43:39 -0700

    Adds tests for encounter vms.

---
+++ test/viewmodels/dm/test_encounter_detail.js
@@ -0,0 +1,106 @@
+'use strict';
+
+describe('EncounterDetailViewModel', function(){
+    //Clean up after each test.
+    afterEach(function() {
+        simple.restore();
+    });
+
+    describe('Load', function() {
+        it('should load encounter\'s data', function() {
+            var vm = new EncounterDetailViewModel(new Encounter());
+            var setupSpy = simple.mock(vm, '_setupSectionVMs').callFn(function() {});
+            var initSpy = simple.mock(ViewModelUtilities, 'initSubViewModels').callFn(function() {});
+            var loadSpy = simple.mock(ViewModelUtilities, 'loadSubViewModels').callFn(function() {});
+
+            setupSpy.called.should.equal(false);
+            initSpy.called.should.equal(false);
+            loadSpy.called.should.equal(false);
+
+            vm.load();
+
+            setupSpy.called.should.equal(true);
+            initSpy.called.should.equal(true);
+            loadSpy.called.should.equal(true);
+        });
+    });
+
+    describe('Unload', function() {
+        it('should unload encounter\'s data', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var vm = new EncounterDetailViewModel(new Encounter());
+            var saveSpy = simple.mock(vm, 'save').callFn(function() {});
+            var unloadSpy = simple.mock(ViewModelUtilities, 'unloadSubViewModels').callFn(function() {});
+
+            saveSpy.called.should.equal(false);
+            unloadSpy.called.should.equal(false);
+
+            vm.unload();
+
+            saveSpy.called.should.equal(true);
+            unloadSpy.called.should.equal(true);
+        });
+    });
+
+    describe('modalFinishedOpening', function() {
+        it('should set the focus of the open modal', function() {
+            var vm = new EncounterDetailViewModel(new Encounter());
+            vm.modalFinishedOpening();
+            vm.nameHasFocus().should.equal(true);
+        });
+    });
+
+    describe('modalFinishedClosing', function() {
+        it('should clear the value of the modal open var and save', function() {
+            var vm = new EncounterDetailViewModel(new Encounter());
+            var saveSpy = simple.mock(vm, 'save').callFn(function() {});
+            vm.modalFinishedClosing();
+            vm.openModal().should.equal(false);
+            saveSpy.called.should.equal(true);
+        });
+    });
+
+    describe('toggleModal', function() {
+        it('should toggle the value of the openModal Var', function() {
+            var vm = new EncounterDetailViewModel(new Encounter());
+            vm.openModal().should.equal(false);
+            vm.toggleModal();
+            vm.openModal().should.equal(true);
+            vm.toggleModal();
+            vm.openModal().should.equal(false);
+        });
+    });
+
+    // Private Methods
+
+    describe('_setupSectionVMs', function() {
+        it('should create child view models for each section and initialize them.', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var vm = new EncounterDetailViewModel(new Encounter());
+            simple.mock(PersistenceService, 'findFirstBy').callFn(function(model, attr, value) {
+                return new model();
+            });
+
+            vm.sections.forEach(function(section, idx, _) {
+                Should.not.exist(vm[section.property]());
+                vm._setupSectionVMs();
+                Should.exist(vm[section.property]());
+            });
+        });
+        it('should create child view models for each section and initialize them.', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var vm = new EncounterDetailViewModel(new Encounter());
+            simple.mock(PersistenceService, 'findFirstBy').callFn(function(model, attr, value) {
+                return new model();
+            });
+
+            vm.sections.push({ property: 'fakeSectionViewModel', vm: function(){}, model: function(){} });
+            try {
+                vm._setupSectionVMs();
+                (false).should.equal(true);
+            } catch(err) {
+                (true).should.equal(true);
+            }
+        });
+    });
+});
