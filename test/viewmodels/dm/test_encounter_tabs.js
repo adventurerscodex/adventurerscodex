6175d4f9afb209e2e2eeb59e1289e259c9000297
Brian Schrader <brian@brianschrader.com>
2016-11-03 17:43:39 -0700

    Adds tests for encounter vms.

---
+++ test/viewmodels/dm/test_encounter_tabs.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('EncounterTabViewModel', function(){
+    //Clean up after each test.
+    afterEach(function() {
+        simple.restore();
+    });
+
+    describe('Init', function() {
+        it('should init the sub view models', function() {
+            var vm = new EncounterTabViewModel();
+            var notifySpy = simple.mock(ViewModelUtilities, 'initSubViewModels');
+
+            vm.init();
+
+            notifySpy.called.should.equal(true);
+        });
+    });
+
+    describe('Load', function() {
+        it('should load the sub view models', function() {
+            var vm = new EncounterTabViewModel();
+            simple.mock(ViewModelUtilities, 'loadSubViewModels').returnWith(true);
+            var notifySpy = simple.mock(ViewModelUtilities, 'loadSubViewModels');
+
+
+            vm.load();
+
+            notifySpy.called.should.equal(true);
+        });
+    });
+
+    describe('Unload', function() {
+        it('should unload the sub view models', function() {
+            var vm = new EncounterTabViewModel();
+            var notifySpy = simple.mock(ViewModelUtilities, 'unloadSubViewModels');
+
+            vm.unload();
+
+            notifySpy.called.should.equal(true);
+        });
+    });
+});
