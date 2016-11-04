07bd16f600455a12f7f3f12e858c63a60463daa3
tanyxp <nathaniel.arellano@gmail.com>
2016-11-03 22:41:59 -0700

    Added extender file

---
+++ charactersheet/bin/knockout-required-extender.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ *
+ */
+
+ko.extenders.required = function(target, overrideMessage) {
+    //add some sub-observables to our observable
+    target.hasError = ko.observable();
+    target.validationMessage = ko.observable();
+
+    //define a function to do validation
+    function validate(newValue) {
+       target.hasError(newValue ? false : true);
+       target.validationMessage(newValue ? "" : overrideMessage || "This field is required");
+    }
+
+    //initial validation
+    validate(target());
+
+    //validate whenever the value changes
+    target.subscribe(validate);
+
+    //return the original observable
+    return target;
+};
