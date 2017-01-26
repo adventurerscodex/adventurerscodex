6c40437991da5b17e5d9d2cad5164c307d65df37
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2017-01-25 19:33:34 -0800

    toastr (#1161)
    * Added toastr. Need to document use and fix tests.
    
    * Fixed tests.
    
    * Added comments, removed preventDuplicate flag, remved subscriptions from init.

--- charactersheet/charactersheet/viewmodels/common/user_notification.js
+++ charactersheet/charactersheet/viewmodels/common/user_notification.js
@@ -7,43 +7,55 @@
 function UserNotificationViewModel() {
     var self = this;
 
-    self.notifications = ko.observableArray([]);
-
     self.init = function() {
+    };
+
+    self.load = function() {
         Notifications.userNotification.infoNotification.add(self.addInfoNotification);
         Notifications.userNotification.successNotification.add(self.addSuccessNotification);
         Notifications.userNotification.warningNotification.add(self.addWarningNotification);
         Notifications.userNotification.dangerNotification.add(self.addDangerNotification);
+                
+        toastr.options = {
+            'closeButton': true,
+            'debug': false,
+            'newestOnTop': true,
+            'progressBar': true,
+            'positionClass': 'toast-top-right',
+            'preventDuplicates': false,
+            'onclick': null,
+            'showDuration': '300',
+            'hideDuration': '1000',
+            'timeOut': '10000',
+            'extendedTimeOut': '10000',
+            'showEasing': 'swing',
+            'hideEasing': 'linear',
+            'showMethod': 'fadeIn',
+            'hideMethod': 'fadeOut'
+        };
     };
-
-    self.load = function() {};
 
     self.unload = function() {};
 
-    self.visibleNotifications = ko.pureComputed(function() {
-        return self.notifications().reverse().filter(function(e, i, _) {
-            return e.visible();
-        })[0];
-    });
-
-    self.addInfoNotification = function(message) {
-        self.notifications.push(
-            UserNotification.notificationWithTypeAndMessage('info', message));
+    /**
+     * Dispatches notification to toastr.
+     * 
+     * @param message  body of notification
+     * @param title  title of notification, can be blank.
+     */
+    self.addInfoNotification = function(message, title) {
+        toastr.info(message, title);
     };
 
-    self.addSuccessNotification = function(message) {
-        self.notifications.push(
-            UserNotification.notificationWithTypeAndMessage('success', message));
+    self.addSuccessNotification = function(message, title) {
+        toastr.success(message, title);
     };
 
-    self.addWarningNotification = function(message) {
-        self.notifications.push(
-            UserNotification.notificationWithTypeAndMessage('warning', message));
+    self.addWarningNotification = function(message, title) {
+        toastr.warning(message, title);
     };
 
-    self.addDangerNotification = function(message) {
-        self.notifications.push(
-            UserNotification.notificationWithTypeAndMessage('danger', message));
+    self.addDangerNotification = function(message, title) {
+        toastr.error(message, title);
     };
-
 }
