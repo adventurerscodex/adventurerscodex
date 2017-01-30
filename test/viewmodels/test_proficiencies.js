9862a83207b2a5bbad5cd5dbad23800c8408b7d2
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2017-01-30 10:35:59 -0800

    Added vm tests.

--- test/viewmodels/test_proficiencies.js
+++ test/viewmodels/test_proficiencies.js
@@ -1,97 +1,158 @@
 'use strict';
 
-describe('Proficiency Tree', function() {
-    var delShim = function() {};
+describe('ProficienciesViewModel', function() {
+    //Clean up after each test.
+    afterEach(function() {
+        simple.restore();
+    });
 
-    describe('Add skill', function() {
-        it('should add a new skill to the list of proficiencies', function() {
+    describe('Add proficiency', function() {
+        it('should add a new proficiency to the list of proficiencies', function() {
             simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
 
-            var p = new ProficienciesViewModel();
-            p.proficiencies().length.should.equal(0);
-            p.addProficiency();
-            p.proficiencies().length.should.equal(1);
+            var proficienciesViewModel = new ProficienciesViewModel();
+            proficienciesViewModel.proficiencies().length.should.equal(0);
+            proficienciesViewModel.addProficiency();
+            proficienciesViewModel.proficiencies().length.should.equal(1);
         });
     });
 
-    describe('Remove skill', function() {
-        it('should remove a skill from the list of proficiencies', function() {
-            var c = CharacterManager.activeCharacter;
-            CharacterManager.activeCharacter = function() {
-                return {
-                    key: function() { return '1234'; }
-                };
-            };
+    describe('Remove proficiency', function() {
+        it('should remove a proficiency from the list of proficiencies', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
 
-            var p = new ProficienciesViewModel();
-            p.proficiencies().length.should.equal(0);
-            p.blankSkill().delete = delShim;
-            p.addProficiency();
-            p.proficiencies().length.should.equal(1);
-            p.removeSkill(p.proficiencies().pop());
-            p.proficiencies().length.should.equal(0);
-
-            CharacterManager.activeCharacter = c;
+            var proficienciesViewModel = new ProficienciesViewModel();
+            proficienciesViewModel.proficiencies().length.should.equal(0);
+            proficienciesViewModel.addProficiency();
+            proficienciesViewModel.proficiencies().length.should.equal(1);
+            proficienciesViewModel.removeProficiency(proficienciesViewModel.proficiencies().pop());
+            proficienciesViewModel.proficiencies().length.should.equal(0);
         });
     });
 
-    describe('Edit skill', function() {
-        it('should put a skill from the list of proficiencies into the selected slot', function() {
-            var c = CharacterManager.activeCharacter;
-            CharacterManager.activeCharacter = function() {
-                return {
-                    key: function() { return '1234'; }
-                };
-            };
+    describe('Edit proficiency', function() {
+        it('should put a proficiency from the list of proficiencies into the selected slot', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
 
-            var p = new ProficienciesViewModel();
-            p.addProficiency();
-            Should.not.exist(p.selecteditem());
-            p.proficiencies().length.should.equal(1);
-            p.editSkill(p.proficiencies()[0]);
-            p.selecteditem().should.equal(p.proficiencies.pop());
-
-            CharacterManager.activeCharacter = c;
+            var proficienciesViewModel = new ProficienciesViewModel();
+            proficienciesViewModel.addProficiency();
+            Should.not.exist(proficienciesViewModel.selecteditem());
+            proficienciesViewModel.proficiencies().length.should.equal(1);
+            proficienciesViewModel.editProficiency(proficienciesViewModel.proficiencies()[0]);
+            proficienciesViewModel.selecteditem().should.equal(proficienciesViewModel.proficiencies.pop());
         });
     });
+
+    describe('Load', function() {
+        it('should load the saved list of proficiencies', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            simple.mock(PersistenceService, 'findBy').returnWith([new Proficiency()]);
+
+            var proficienciesViewModel = new ProficienciesViewModel();
+            proficienciesViewModel.proficiencies().length.should.equal(0);
+            proficienciesViewModel.load();
+            proficienciesViewModel.proficiencies().length.should.equal(1);
+        });
+    });
+
+    describe('Unload', function() {
+        it('should unload the saved list of proficiencies', function() {
+            var notificationSpy = simple.mock(Notifications.global.save, 'remove');
+
+            var proficienciesViewModel = new ProficienciesViewModel();
+            notificationSpy.called.should.equal(false);
+            proficienciesViewModel.unload();
+            notificationSpy.called.should.equal(true);
+        });
+    });
+
+    describe('modalFinishedOpening', function() {
+        it('perform actions after a modal has opened', function() {
+            var proficienciesViewModel = new ProficienciesViewModel();
+
+            proficienciesViewModel.shouldShowDisclaimer(false);
+            proficienciesViewModel.firstModalElementHasFocus(false);
+            proficienciesViewModel.modalFinishedOpening();
+            proficienciesViewModel.shouldShowDisclaimer().should.equal(false);
+            proficienciesViewModel.firstModalElementHasFocus().should.equal(true);
+        });
+    });    
+
+    describe('modalFinishedClosing', function() {
+        it('perform actions after a modal has closed', function() {
+            var proficienciesViewModel = new ProficienciesViewModel();
+
+            proficienciesViewModel.previewTabStatus('');
+            proficienciesViewModel.editTabStatus('active');
+            proficienciesViewModel.modalFinishedClosing();
+            proficienciesViewModel.previewTabStatus().should.equal('active');
+            proficienciesViewModel.editTabStatus().should.equal('');
+        });
+    });
+
+    describe('selectPreviewTab', function() {
+        it('perform actions after the preview tab has been selected', function() {
+            var proficienciesViewModel = new ProficienciesViewModel();
+
+            proficienciesViewModel.previewTabStatus('');
+            proficienciesViewModel.editTabStatus('active');
+            proficienciesViewModel.selectPreviewTab();
+            proficienciesViewModel.previewTabStatus().should.equal('active');
+            proficienciesViewModel.editTabStatus().should.equal('');
+        });
+    });       
+
+    describe('selectEditTab', function() {
+        it('perform actions after the preview tab has been selected', function() {
+            var proficienciesViewModel = new ProficienciesViewModel();
+
+            proficienciesViewModel.previewTabStatus('active');
+            proficienciesViewModel.editTabStatus('');
+            proficienciesViewModel.selectEditTab();
+            proficienciesViewModel.previewTabStatus().should.equal('');
+            proficienciesViewModel.editTabStatus().should.equal('active');
+            proficienciesViewModel.editFirstModalElementHasFocus().should.equal(true);
+        });
+    });             
 
     describe('Sort By', function() {
         it('should sort the list of proficiencies by given criteria', function() {
-            var p = new ProficienciesViewModel();
-            p.sortBy('name');
-            p.sort().should.equal(p.sorts['name desc']);
-            p.sortBy('name');
-            p.sort().should.equal(p.sorts['name asc']);
-            p.sortBy('modifier');
-            p.sort().should.equal(p.sorts['modifier asc']);
-            p.sortBy('modifier');
-            p.sort().should.equal(p.sorts['modifier desc']);
+            var proficienciesViewModel = new ProficienciesViewModel();
+            proficienciesViewModel.sortBy('name');
+            proficienciesViewModel.sort().should.equal(proficienciesViewModel.sorts['name desc']);
+            proficienciesViewModel.sortBy('name');
+            proficienciesViewModel.sort().should.equal(proficienciesViewModel.sorts['name asc']);
+            proficienciesViewModel.sortBy('type');
+            proficienciesViewModel.sort().should.equal(proficienciesViewModel.sorts['type asc']);
+            proficienciesViewModel.sortBy('type');
+            proficienciesViewModel.sort().should.equal(proficienciesViewModel.sorts['type desc']);
         });
     });
 
     describe('Sort Arrow', function() {
         it('should sort the list of proficiencies by given criteria', function() {
-            var p = new ProficienciesViewModel();
-            p.sortBy('name');
-            p.sort().should.equal(p.sorts['name desc']);
-            p.sortArrow('name').should.equal('fa fa-arrow-down fa-color');
-            p.sortArrow('modifier').should.equal('');
-            p.sortBy('name');
-            p.sort().should.equal(p.sorts['name asc']);
-            p.sortArrow('name').should.equal('fa fa-arrow-up fa-color');
-            p.sortArrow('modifier').should.equal('');
+            var proficienciesViewModel = new ProficienciesViewModel();
+            proficienciesViewModel.sortBy('name');
+            proficienciesViewModel.sort().should.equal(proficienciesViewModel.sorts['name desc']);
+            proficienciesViewModel.sortArrow('name').should.equal('fa fa-arrow-down fa-color');
+            proficienciesViewModel.sortArrow('type').should.equal('');
+            proficienciesViewModel.sortBy('name');
+            proficienciesViewModel.sort().should.equal(proficienciesViewModel.sorts['name asc']);
+            proficienciesViewModel.sortArrow('name').should.equal('fa fa-arrow-up fa-color');
+            proficienciesViewModel.sortArrow('type').should.equal('');
         });
     });
 
     describe('Clear', function() {
         it('should clear all the values in proficiencies.', function() {
-            var p = new ProficienciesViewModel();
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var proficienciesViewModel = new ProficienciesViewModel();
             var proficiencies = [new Proficiency()];
-            proficiencies[0].delete = delShim;
-            p.proficiencies(proficiencies);
-            p.proficiencies().should.equal(proficiencies);
-            p.clear();
-            p.proficiencies().length.should.equal(0);
+
+            proficienciesViewModel.addProficiency();
+            proficienciesViewModel.proficiencies().length.should.equal(1);
+            proficienciesViewModel.clear();
+            proficienciesViewModel.proficiencies().length.should.equal(0);
         });
     });
 });
