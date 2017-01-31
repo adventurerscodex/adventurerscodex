9e8e0478de0dfba126d5376abdece63f3394804c
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2017-01-30 16:25:40 -0800

    Added tests for encounter section VMs.

--- test/viewmodels/dm/test_notes_section.js
+++ test/viewmodels/dm/test_notes_section.js
@@ -7,33 +7,73 @@ describe('NotesSectionViewModel', function(){
     });
 
     describe('Load', function() {
-        it('should load notes model', function() {
-            var notes = new NotesSection();
-            notes.notes('blah');
-            notes.visible(false);
+        it('should load model and section', function() {
             simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
-            var vm = new NotesSectionViewModel(new Encounter(), notes);
-
+            var vm = new NotesSectionViewModel(new Encounter());
+            var spy1 = simple.mock(Notifications.global.save, 'add');
+            var spy2 = simple.mock(Notifications.encounters.changed, 'add');
+            simple.mock(PersistenceService, 'findFirstBy').returnWith(null);
+            
             vm.load();
-        });
-        it('should receive a bad input and create a new notesSection', function() {
-            var encounter = new Encounter();
-            encounter.encounterId('1234');
-            var vm = new NotesSectionViewModel(encounter, null);
-            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
 
-            vm.load();
+            spy1.called.should.equal(true);
+            spy2.called.should.equal(true);
         });
-    });
+    });  
 
     describe('Unload', function() {
-        it('should unload model', function() {
-            var vm = new NotesSectionViewModel(new Encounter(), new NotesSection());
-            vm.notes('asd');
-            vm.visible(false);
-            simple.mock(PersistenceService, 'findFirstBy').returnWith(null);
-
+        it('should unsubscribe from notifications', function() {
+            var vm = new NotesSectionViewModel(new Encounter());
+            var spy1 = simple.mock(Notifications.global.save, 'remove');
+            var spy2 = simple.mock(Notifications.encounters.changed, 'remove');
+            
             vm.unload();
+
+            spy1.called.should.equal(true);
+            spy2.called.should.equal(true);
         });
     });
+
+    describe('Save', function() {
+        it('should save model', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var vm = new NotesSectionViewModel(new Encounter());
+            simple.mock(PersistenceService, 'findFirstBy').returnWith(new NotesSection());
+            
+            vm.save();
+        });
+        it('should create and save a new model', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var vm = new NotesSectionViewModel(new Encounter());
+            simple.mock(PersistenceService, 'findFirstBy').returnWith(null);
+            
+            vm.save();
+        });        
+    });    
+
+    describe('Delete', function() {
+        it('should delete model', function() {
+            var vm = new NotesSectionViewModel(new Encounter());
+            simple.mock(PersistenceService, 'findFirstBy').returnWith(new NotesSection());
+            
+            vm.delete();
+        });
+    });  
+
+    describe('Data has changed', function() {
+        it('should load new data', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var vm = new NotesSectionViewModel(new Encounter());
+            simple.mock(PersistenceService, 'findFirstBy').returnWith(null);
+            
+            vm._dataHasChanged();
+        });
+        it('should load new data', function() {
+            simple.mock(CharacterManager, 'activeCharacter').callFn(MockCharacterManager.activeCharacter);
+            var vm = new NotesSectionViewModel(new Encounter());
+            simple.mock(PersistenceService, 'findFirstBy').returnWith(new NotesSection());
+            
+            vm._dataHasChanged();
+        });        
+    });         
 });
