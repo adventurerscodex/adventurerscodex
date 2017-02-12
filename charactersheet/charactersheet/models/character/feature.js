503f8c05b9120b96b1c82b197ae7f6de326ed62c
Jim Krooskos <jkrooskos@users.noreply.github.com>
2017-02-12 12:13:09 -0800

    Add features functionality (#1195)
    * Add features functionality
    
    * Add level and character class
    
    * Add prepop for features

---
+++ charactersheet/charactersheet/models/character/feature.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function Feature() {
+    var self = this;
+
+    self.ps = PersistenceService.register(Feature, self);
+    self.mapping = {
+        include: ['characterId', 'name', 'description', 'trackedId', 'isTracked',
+                  'level', 'characterClass', 'tracked']
+    };
+
+    self.characterId = ko.observable(null);
+    self.name = ko.observable('');
+    self.level = ko.observable('');
+    self.characterClass = ko.observable('');
+    self.description = ko.observable('');
+    self.trackedId = ko.observable(null);
+    self.isTracked = ko.observable(false);
+    self.tracked = ko.observable(null);
+
+    self.save = function() {
+        self.ps.save();
+    };
+
+    self.delete = function() {
+        self.ps.delete();
+    };
+
+    self.clear = function() {
+        var values = new Feature().exportValues();
+        var mapping = ko.mapping.autoignore(self, self.mapping);
+        ko.mapping.fromJS(values, mapping, self);
+    };
+
+    self.importValues = function(values) {
+        var mapping = ko.mapping.autoignore(self, self.mapping);
+        ko.mapping.fromJS(values, mapping, self);
+    };
+
+    self.exportValues = function() {
+        var mapping = ko.mapping.autoignore(self, self.mapping);
+        return ko.mapping.toJS(self, mapping);
+    };
+}
--- charactersheet/charactersheet/utilities/data_repository.js
+++ charactersheet/charactersheet/utilities/data_repository.js
@@ -1,6 +1,16 @@
 'use strict';
+
 var DataRepository = {
-    spells : null,
-    items : null,
-    magicItems : null
+    /**
+     * Convenience function to filter data repository by key
+     *
+     * @param arrayName: name of array to be queried
+     * @param key: the key to filter by
+     * @param value: value that key should equal
+     */
+    filterBy: function(arrayName, key, value){
+        return DataRepository[arrayName].filter(function(item, idx, _) {
+            return item[key] === value;
+        });
+    }
 };
