7b83223a0bd8b9ec6e8a23087c2cd50843df3c4f
Brian Schrader <brian@brianschrader.com>
2017-02-12 10:45:44 -0800

    Spruces up logic for login and auth to use new auth api and new urls.

--- charactersheet/charactersheet/services/common/authentication_service.js
+++ charactersheet/charactersheet/services/common/authentication_service.js
@@ -16,6 +16,8 @@ var AuthenticationServiceManager = new SharedServiceManager(_AuthenticationServi
 function _AuthenticationService(config) {
     var self = this;
 
+    self.validationUrl = '/api/o/validate/?access_token={access_token}'
+
     /**
      * A configuration object that can be used to set options at initialization-time.
      * Changes to this object after such time requires a rebuild of the service.
@@ -23,14 +25,28 @@ function _AuthenticationService(config) {
     self.config = config;
 
     self.init = function() {
-        // Get the fragments from the current URL.
+        // Make sure the token we have is valid.
         var fragments = (new URI()).fragment(true);
+        var url = self.validationUrl.replace('{access_token}', fragments['access_token']);
+        $.getJSON(url, self._handleValidationResponse);
+    };
 
+    // Private Methods
+
+    self._handleValidationResponse = function(data, status) {
+        if (status !== '200' || data.code !== 0) {
+            return;
+        }
+
+        var fragments = (new URI()).fragment(true);
         var token = PersistenceService.findAll(AuthenticationToken)[0];
         if (!token) {
             token = new AuthenticationToken();
         }
+
         token.map(fragments);
         token.save();
+
+        Notifications.authentication.loggedIn.dispatch();
     };
 }
