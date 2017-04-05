7e0ce7f15fa08419be427e5d9d3e2982fbd7a936
Brian Schrader <brian@brianschrader.com>
2017-04-05 16:07:54 -0700

    Adds new build workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt);
@@ -32,7 +30,7 @@ module.exports = function(grunt) {
             },
             deploy: {
                 files: {
-                    'charactersheet/charactersheet/app.js': [
+                    '_build/charactersheet/app.js': [
                         'charactersheet/bower_components/Strophe.js/strophe.js',
                         'charactersheet/bower_components/jquery/dist/jquery.min.js',
                         'charactersheet/bower_components/jquery-ui/jquery-ui.min.js',
@@ -55,13 +53,43 @@ module.exports = function(grunt) {
                 }
             }
         },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'charactersheet/',
+                    src: [
+                        '*.html',
+                        'bower_components/**/fonts/*',
+                        'images/*',
+                        'images/**/*',
+                        'charactersheet/**/*.html',
+                    ],
+                    dest: '_build/'
+                }],
+            },
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'charactersheet/',
+                    src: [
+                        '*.css',
+                        'bower_components/**/*.css',
+                        'bin/**/*.css',
+                    ],
+                    dest: '_build',
+                }]
+            }
+        },
         watch: {
             scripts: {
                 files: [
                     'charactersheet/charactersheet/**/*.js',
                     'charactersheet/charactersheet/*.js'
                 ],
-                tasks: ['uglify'],
+                tasks: ['copy', 'uglify'],
                 options: {
                     spawn: false,
                     interrupt: true
@@ -70,9 +98,12 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.registerTask('default', ['eslint']);
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-karma-coveralls');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('build', ['uglify', 'cssmin', 'copy']);
 };
