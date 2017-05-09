ac916ef6e0fa22e85c0d123c3e1ec6b2d932f532
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2017-05-08 18:06:17 -0700

    Adds api updates, changes columns.

--- charactersheet/charactersheet/utilities/convenience.js
+++ charactersheet/charactersheet/utilities/convenience.js
@@ -11,7 +11,8 @@
 var Utility = {
     markdown: {},
     string: {},
-    array: {}
+    array: {},
+    oauth: {}
 };
 
 
@@ -60,5 +61,34 @@ Utility.array.updateElement = function(array, updatedElement, elementId) {
         if (element.__id === elementId) {
             element.importValues(updatedElement.exportValues());
         }
+    });
+};
+
+/* OAuth Request Shortcuts */
+
+/**
+ * Using the first Access Token in the local data store, set the
+ * headers for an OAuth request.
+ */
+Utility.oauth.setXHRBearerHeader = function(xhr) {
+    var key = CharacterManager.activeCharacter().key();
+    var token = PersistenceService.findAll(AuthenticationToken)[0];
+    if (!token) {
+        throw Error('No token available for OAuth Requests.');
+    }
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token.accessToken());
+};
+
+/**
+ * Mimics $.getJSON, but includes OAuth Headers.
+ */
+Utility.oauth.getJSON = function(url, onsuccess, onerror) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: onsuccess,
+        error: onerror,
+        beforeSend: Utility.oauth.setXHRBearerHeader
     });
 };
