2553122d4c63684b3d7051cb9613cc279b68cbed
tanyxp <nathaniel.arellano@gmail.com>
2017-05-15 19:42:20 -0700

    Starts implementation of dropbox saver.

--- charactersheet/charactersheet/settings.js
+++ charactersheet/charactersheet/settings.js
@@ -36,7 +36,7 @@ var Settings = {
     /**
      * A config object for dropbox integration.
      */
-    dropboxConfigOptions: {
+    dropboxPickerConfigOptions: {
         // Required. Called when a user selects an item in the Chooser.
         success: function(files) {
             WizardIntroStepViewModel.importRemoteFile(files);
@@ -56,6 +56,34 @@ var Settings = {
         // file types, such as "video" or "images" in the list. For more information,
         // see File types below. By default, all extensions are allowed.
         extensions: ['.json']
+    },
+
+    options: {
+        files: [
+
+        ],
+
+        // Success is called once all files have been successfully added to the user's
+        // Dropbox, although they may not have synced to the user's devices yet.
+        success: function () {
+            Notifications.userNotification.successNotification.dispatch('Success! File saved to your Dropbox.', '');
+        },
+
+        // Progress is called periodically to update the application on the progress
+        // of the user's downloads. The value passed to this callback is a float
+        // between 0 and 1. The progress callback is guaranteed to be called at least
+        // once with the value 1.
+        progress: function (progress) {},
+
+        // Cancel is called if the user presses the Cancel button or closes the Saver.
+        cancel: function () {},
+
+        // Error is called in the event of an unexpected response from the server
+        // hosting the files, such as not being able to find a file. This callback is
+        // also called if there is an error on Dropbox or if the user is over quota.
+        error: function (errorMessage) {
+            Notifications.userNotification.dangerNotification.dispatch('There was an error exporting your character or campaign to Dropbox. Please try again.','');
+        }
     },
 
     /**
