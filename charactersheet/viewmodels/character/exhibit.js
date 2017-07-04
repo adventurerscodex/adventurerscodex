90e6ddbc09d87fe511baad55720a0cd178a9517b
Brian Schrader <brian@brianschrader.com>
2017-07-04 14:19:15 -0700

    Hotfix

--- charactersheet/viewmodels/character/exhibit.js
+++ charactersheet/viewmodels/character/exhibit.js
@@ -6,23 +6,41 @@ function ExhibitViewModel() {
     self.name = ko.observable('');
     self.url = ko.observable('');
 
+    self._isConnectedToParty = ko.observable(false);
+
     self.load = function() {
+        Notifications.party.joined.add(self._connectionHasChanged);
         Notifications.xmpp.routes.pcard.add(self.exhibitGivenImage);
     };
 
     self.unload = function() {
+        Notifications.party.joined.remove(self._connectionHasChanged);
         Notifications.xmpp.routes.pcard.remove(self.exhibitGivenImage);
     };
 
     self.exhibitGivenImage = function(newPCard) {
         var image = pCard.fromEntries(newPCard).get('exhibitImage')[0];
-        if (image) {
+        if (self._isConnectedToParty() && image) {
             self.name(image.name);
             self.url(Utility.string.createDirectDropboxLink(image.url));
-            Notifications.userNotification.infoNotification.dispatch('New image on Exhibit', '');
+
+            if (!self._imageIsIdentical(image)) {
+                Notifications.userNotification.infoNotification.dispatch('New image on Exhibit', '');
+            }
         } else {
             self.name('');
             self.url('');
         }
+    };
+
+    /* Private Methods */
+
+    self._connectionHasChanged = function() {
+        var chat = ChatServiceManager.sharedService();
+        self._isConnectedToParty(chat.currentPartyNode != null);
+    };
+
+    self._imageIsIdentical = function(image) {
+        return self.name == image.name && self.url == image.url;
     };
 }
