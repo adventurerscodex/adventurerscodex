3c67ae2157d83f90ab5683ef7b12f1726ef9d48b
Brian Schrader <brian@brianschrader.com>
2017-07-26 21:49:13 -0700

    Adds notifications service (#1507)
    * Adds notifications service
    
    - Supports new persistent and temp notifications
    - Adds new OAuth PUT method
    
    * Fixes linting

---
+++ charactersheet/charactersheet/services/common/account/notification_service.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * A global service that fetches the user's active notifications.
+ */
+var NotificationsServiceManager = new SharedServiceManager(_NotificationsService, null);
+
+/**
+ * An internal service implementation that holds onto data regarding the
+ * account notifications.
+ *
+ * This service contains all of the relevant information for keeping
+ * track of a user's authentication state.
+ */
+function _NotificationsService(config) {
+    var self = this;
+
+    self.url = '/api/notifications.json';
+
+    self.notifications = ko.observableArray([]);
+
+    /**
+     * A configuration object that can be used to set options at initialization-time.
+     * Changes to this object after such time requires a rebuild of the service.
+     */
+    self.config = config;
+
+    self.init = function() {
+        Notifications.authentication.loggedIn.add(self.getNotifications);
+    };
+
+    self.getNotifications = function() {
+        Utility.oauth.getJSON(self.url, self._handleResponse);
+    };
+
+    self.markAsRead = function(status) {
+        Utility.oauth.putData(status.url, { read: true });
+    };
+
+    // Private Methods
+
+    self._handleResponse = function(data, status) {
+        if (status !== 'success') {
+            return;
+        }
+
+        self.notifications(data.results);
+        self.notifications().forEach(function(status) {
+            var options = {
+                onCloseClick: function() {
+                    self.markAsRead(status);
+                },
+                onclick: function() {
+                    self.markAsRead(status);
+                }
+            };
+            if (status.notification.requires_confirmation) {
+                options['timeOut'] = 0;
+                options['extendedTimeOut'] = 0;
+            }
+
+            var notificationType = status.notification.type + 'Notification';
+            Notifications.userNotification[notificationType].dispatch(
+                status.notification.content,
+                status.notification.title,
+                options
+            );
+        });
+    };
+}
