bc48fc273d368ce685a7fb410ea2238029f98275
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2017-08-01 19:58:39 -0700

    Reduces notification spam.

--- charactersheet/charactersheet/viewmodels/character/exhibit.js
+++ charactersheet/charactersheet/viewmodels/character/exhibit.js
@@ -8,14 +8,14 @@ function ExhibitViewModel() {
     self.isConnectedToParty = ko.observable(false);
 
     self.load = function() {
-        Notifications.xmpp.routes.pcard.add(self.exhibitGivenImage);
+        Notifications.party.players.changed.add(self.exhibitGivenImage);
         Notifications.party.left.add(self.checkForParty);
         Notifications.party.joined.add(self.checkForParty);
         self.checkForParty();
     };
 
     self.unload = function() {
-        Notifications.xmpp.routes.pcard.remove(self.exhibitGivenImage);
+        Notifications.party.players.changed.remove(self.exhibitGivenImage);
         Notifications.party.left.remove(self.checkForParty);
         Notifications.party.joined.remove(self.checkForParty);
     };
@@ -25,14 +25,26 @@ function ExhibitViewModel() {
         self.isConnectedToParty(chat.currentPartyNode == null ? false : true);
     };
 
-    self.exhibitGivenImage = function(newPCard) {
-        var image = pCard.fromEntries(newPCard).get('exhibitImage')[0];
-        var playerType = pCard.fromEntries(newPCard).get('playerType')[0];
+    self.exhibitGivenImage = function(pcards) {
+        // Ignore non-DM cards and player is in a party
+        if (!self.isConnectedToParty()) { return; }
+        var playerType;
+        var image;
+        var isDuplicateImage;
+        pcards.forEach(function(card, idx, _) {
+            playerType = card.get('playerType')[0];
+            if (PlayerTypes.dmPlayerType.key == playerType) {
+                image = card.get('exhibitImage')[0];
+                if (image) {
+                    isDuplicateImage = image.name == self.name() && image.url == self.url();
+                } else {
+                    self.name('');
+                    self.url('');
+                }
+            }
+        });
 
-        // Ignore non-DM cards.
-        if (PlayerTypes.dmPlayerType.key != playerType) { return; }
-
-        if (image) {
+        if (image && !isDuplicateImage) {
             self.name(image.name);
             self.url(Utility.string.createDirectDropboxLink(image.url));
             Notifications.userNotification.infoNotification.dispatch('New image on Exhibit', '');
