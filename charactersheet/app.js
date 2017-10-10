dd9c8e61466fceea1adf60c6a030d3da0c6d6ba1
Brian Schrader <brian@brianschrader.com>
2017-10-09 17:29:54 -0700

    Reorders App imports.

--- charactersheet/app.js
+++ charactersheet/app.js
@@ -3,13 +3,14 @@ import ko from 'knockout'
 import 'bootstrap'
 import 'font-awesome-webpack'
 
+// Load Global KO Bindings
+import 'bin/knockout-jquery-autocomplete'
+
 import 'charactersheet/components'
 import 'charactersheet/migrations'
 import 'charactersheet/models'
 import 'charactersheet/services'
 import 'charactersheet/utilities'
-
-import 'bin/knockout-jquery-autocomplete'
 
 import { AdventurersCodexViewModel, template } from 'charactersheet/viewmodels/common/root'
 import template from 'charactersheet/viewmodels/common/root/index.html'
