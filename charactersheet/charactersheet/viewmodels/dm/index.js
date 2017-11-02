aa67424f22bfe4d09d7af48bab2b9f701a9e18a4
Brian Schrader <brian@brianschrader.com>
2017-11-02 13:44:52 -0700

    Fixes issue where DM sections wouldnt load

--- charactersheet/charactersheet/viewmodels/dm/index.js
+++ charactersheet/charactersheet/viewmodels/dm/index.js
@@ -8,5 +8,6 @@ export { CampaignOverviewViewModel } from 'charactersheet/viewmodels/dm/overview
 export { PartyViewModel } from 'charactersheet/viewmodels/dm/party'
 export { PartyStatusLineViewModel } from 'charactersheet/viewmodels/dm/status_line'
 export { DMScreenViewModel } from './dmscreen'
+export { DMRootViewModel } from './root'
 export { PlayerPushModal } from 'charactersheet/viewmodels/dm/player_push_modal'
 export * from 'charactersheet/viewmodels/dm/encounter_sections'
