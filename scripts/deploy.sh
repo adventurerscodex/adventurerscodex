78d480c2bac9180dc1cdbf8056585c2a14ec762c
Brian Schrader <brian@brianschrader.com>
2017-11-25 20:49:45 -0800

    Adds host check toggle function

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,16 @@
 # Since: 2017-11-25
 
 
+add_hosts() {
+    ssh -o "StrictHostKeyChecking=no" "$DEPLOY_HOST"
+}
+
+
 do_push() {
     URL="$1"
 
-    git remote add deploy "$URL"
-    git push -u -f deploy "$TRAVIS_BRANCH"
+    git remote add throwaway "$URL"
+    git push -u -f throwaway "$TRAVIS_BRANCH"
 }
 
 
@@ -19,6 +24,8 @@ if [[ "$TRAVIS_BRANCH" -eq "develop" ]]; then
         echo "No value for variable NIGHTLY_URL was set. Nowhere to push."
         exit 0;
     fi
+
+    add_hosts
 
     do_push $NIGHTLY_URL
     exit 0;
@@ -29,6 +36,8 @@ if [[ "$TRAVIS_BRANCH" -eq "master" ]]; then
         echo "No value for variable MASTER_URL was set. Nowhere to push."
         exit 0;
     fi
+
+    add_hosts
 
     do_push $MASTER_URL
     exit 0;
