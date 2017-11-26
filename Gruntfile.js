a6de5953625643bcc94f32f4806095fef314a0cf
Jim Krooskos <krooskos@gmail.com>
2017-11-26 01:04:26 -0800

    Convert tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,13 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   grunt.initConfig({
 
-	coveralls: {
-		options: {
-			coverageDir: 'coverage',
-			force: true,
-			recursive: true
-		}
-	}
+  coveralls: {
+    options: {
+      coverageDir: 'coverage',
+      force: true,
+      recursive: true
+    }
+  }
   });
 
   grunt.loadNpmTasks('grunt-karma-coveralls');
