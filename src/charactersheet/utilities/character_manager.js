641e933b2729b3b36fbdee269cea1bbc1b0b1b36
Brian Schrader <brian@brianschrader.com>
2017-12-03 21:22:19 -0800

    Adds URL based Character Manager.

--- src/charactersheet/utilities/character_manager.js
+++ src/charactersheet/utilities/character_manager.js
@@ -3,28 +3,62 @@
 import { Character } from 'charactersheet/models/common/character';
 import { Notifications } from 'charactersheet/utilities/notifications';
 import { PersistenceService } from 'charactersheet/services/common/persistence_service';
+import URI from 'urijs';
+
 
 export var CharacterManager = {
-    __activeCharacter__: null
-};
+    __activeCharacter__: null,
+    CHARACTER_ID_FRAGMENT_KEY: 'c',
 
-CharacterManager.changeCharacter = function(characterId) {
-    var newChar = PersistenceService.findFirstBy(Character, 'key', characterId);
-    try {
-        Notifications.characterManager.changing
-            .dispatch(CharacterManager.activeCharacter(), newChar);
-        CharacterManager.__activeCharacter__ = newChar;
-        Notifications.characterManager.changed
-            .dispatch(CharacterManager.activeCharacter());
-    } catch(err) {
-        console.log(err);
-    }
-};
 
-CharacterManager.activeCharacter = function() {
-    if (CharacterManager.__activeCharacter__) {
-        return CharacterManager.__activeCharacter__;
-    } else {
-        return null;
+    init: function() {
+        var fragments = (new URI()).fragment(true);
+        var key = fragments[CharacterManager.CHARACTER_ID_FRAGMENT_KEY];
+        if (key) {
+            var character = PersistenceService.findFirstBy(Character, 'key', key);
+            if (character) {
+                CharacterManager._setActiveCharacter(character);
+            }
+        }
+    },
+
+
+    changeCharacter: function(characterId) {
+        var newCharacter = PersistenceService.findFirstBy(Character, 'key', characterId);
+        try {
+            Notifications.characterManager.changing.dispatch(
+                CharacterManager.activeCharacter(),
+                newCharacter
+            );
+            CharacterManager._setActiveCharacter(newCharacter);
+
+            Notifications.characterManager.changed.dispatch(
+                CharacterManager.activeCharacter()
+            );
+        } catch(err) {
+            console.log(err);
+        }
+    },
+
+
+    activeCharacter: function() {
+        if (CharacterManager.__activeCharacter__) {
+            return CharacterManager.__activeCharacter__;
+        } else {
+            return null;
+        }
+    },
+
+
+    _setActiveCharacter: function(character) {
+        var uri = new URI();
+        uri.removeFragment(CharacterManager.CHARACTER_ID_FRAGMENT_KEY);
+        uri.addFragment(
+            CharacterManager.CHARACTER_ID_FRAGMENT_KEY,
+            character.key()
+        );
+        window.location = uri.toString();
+
+        CharacterManager.__activeCharacter__ = character;
     }
 };
