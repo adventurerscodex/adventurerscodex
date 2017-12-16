8ed45f334a6c79144b0d0f6264faf1148c1a5d10
Brian Schrader <brian@brianschrader.com>
2017-12-16 12:07:55 -0800

    Undo harmful change

--- src/charactersheet/viewmodels/dm/encounter_sections/monster_section/index.js
+++ src/charactersheet/viewmodels/dm/encounter_sections/monster_section/index.js
@@ -168,11 +168,12 @@ export function MonsterSectionViewModel(params) {
         self.editItemIndex = monster.__id;
         self.currentEditItem(new Monster());
         self.currentEditItem().importValues(monster.exportValues());
-        self.currentEditItem().abilityScores(monster.abilityScores().forEach(function(e, i, _) {
-            var abilityScore = new MonsterAbilityScore();
-            abilityScore.importValues(e);
-            return abilityScore;
-        }));
+        self.currentEditItem().abilityScores(monster.abilityScores().map(function(e, i, _) {
+                var abilityScore = new MonsterAbilityScore();
+                abilityScore.importValues(e);
+                return abilityScore;
+            })
+        );
         self.openEditModal(true);
     };
 
