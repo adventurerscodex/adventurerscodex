d05a59e4710009f1ed20ae7bb3ca3123403801a7
Brian Schrader <brian@brianschrader.com>
2017-12-19 19:28:07 -0800

    Fixes issue with Encounter Cells reloading with null and causing error. (#1654)

--- src/charactersheet/viewmodels/dm/encounter_cell/index.js
+++ src/charactersheet/viewmodels/dm/encounter_cell/index.js
@@ -94,10 +94,12 @@ export function EncounterCellViewModel(encounter) {
             new KeyValuePredicate('encounterId', self.encounterId()),
             new KeyValuePredicate('characterId', key)
         ])[0];
-        self.name(encounter.name());
-        self.encounterLocation(encounter.encounterLocation());
-        self.children().forEach(function(child, idx, _) {
-            child.reloadData();
-        });
+        if (encounter) {
+            self.name(encounter.name());
+            self.encounterLocation(encounter.encounterLocation());
+            self.children().forEach(function(child, idx, _) {
+                child.reloadData();
+            });
+        }
     };
 }
