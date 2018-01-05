57ddf3e10d96db119e4c2b712526ba2f99fbe156
Jim Krooskos <jkrooskos@users.noreply.github.com>
2018-01-04 20:30:12 -0800

    Fix death save index error (#1717)
    * Fix death save index error
    
    * Fix spacing and combine if statements

--- src/charactersheet/services/character/status_service_components/healthiness_component.js
+++ src/charactersheet/services/character/status_service_components/healthiness_component.js
@@ -52,13 +52,13 @@ export function HealthinessStatusServiceComponent() {
             var deathSaveFailures = 0;
 
             if (deathSaves.length > 0) {
-                for(var i=0; i<3; i++) {
-                    if(deathSaves[i].deathSaveSuccess()) {
+                for (var i=0; i<3; i++) {
+                    if (deathSaves[i] !== undefined && deathSaves[i].deathSaveSuccess()) {
                         deathSaveSuccesses++;
                     }
                 }
-                for(var j=3; j<6; j++) {
-                    if(deathSaves[j].deathSaveFailure()) {
+                for (var j=3; j<6; j++) {
+                    if (deathSaves[j] !== undefined && deathSaves[j].deathSaveFailure()) {
                         deathSaveFailures++;
                     }
                 }
@@ -143,17 +143,17 @@ export function HealthinessStatusServiceComponent() {
         }
 
         // Character is dead
-        if (health.hitpoints() === 0 && deathSavesDidFail){
+        if (health.hitpoints() === 0 && deathSavesDidFail) {
             return -2;
         }
 
         // Character is unconscious and stable
-        if (health.hitpoints() === 0 && deathSavesDidSucceed){
+        if (health.hitpoints() === 0 && deathSavesDidSucceed) {
             return -1;
         }
 
         // Character is unconscious
-        if (health.hitpoints() === 0){
+        if (health.hitpoints() === 0) {
             return 0.0;
         }
 
