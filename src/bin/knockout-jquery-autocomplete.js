5d5ae8417cb8e525ec97e810ee9ad701b592b5be
Jim Krooskos <jkrooskos@users.noreply.github.com>
2018-01-18 19:41:42 -0800

    Fix autocomplete (#1723)
    * fix autocomplete
    
    * fix css scrolling issue
    
    * Fix remaining autocomplete issues

--- src/bin/knockout-jquery-autocomplete.js
+++ src/bin/knockout-jquery-autocomplete.js
@@ -37,22 +37,16 @@ ko.bindingHandlers.autocomplete = {
         var delay = ko.utils.unwrapObservable(value.delay || 150);
         $(element).autocomplete({
             delay: delay,
-            source: (request, response) => {
-                // Clear the bindings on every search.
-                // https://stackoverflow.com/a/43393889/2085172
-
-                $(element).data('ui-autocomplete').menu.bindings = $();
-                // Call the source as normal.
-                return source(request, response)
-            },
+            source: source,
             minLength: 0,
             select: function(event, ui) {
                 if (onselect) {
                     onselect(ui.item.label, ui.item.value);
                 }
-            }
+            },
         })
         $(element).focus(function() {
+            $(element).data('ui-autocomplete').menu.bindings = $();
             $(element).autocomplete('search', '');
         })
     }
