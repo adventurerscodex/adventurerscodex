4361eff7710543a26b755ad9dbee154f40f38689
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2018-02-27 19:13:28 -0800

    Confirm delete (#1752)
    * Adds confirm delete on character and games modal and character and games picker.
    
    * Fix linting issue.
    
    * Adds the confirm delete to each row not globally.
    
    - Refactors modal to be consistent with picker
    
    * Updates delete button to trashcan
    
    * Update site.css
    
    Removed unused class

--- src/charactersheet/viewmodels/common/character_picker/index.js
+++ src/charactersheet/viewmodels/common/character_picker/index.js
@@ -14,24 +14,28 @@ export function CharacterPickerViewModel(params) {
 
     self.totalLocalStorage = 5; //MB
     self.logo = logo;
-
     self.isLoggedIn = ko.observable(false);
-
-    self.selectedCharacter = ko.observable();
-
     self.characters = ko.observableArray([]);
     self.defaultCharacterKey = ko.observable(null);
     self.state = params.state;
-
-    self.load = function() {
-        self.characters(PersistenceService.findAll(Character));
+    self.deleteCollapse = {
+        // Dynamically built map.
     };
 
-    self.showWizard = function() {
+    self.load = () => {
+        self.characters(PersistenceService.findAll(Character));
+
+        // Build the hash of key -> modal open.
+        self.characters().forEach(({key}) => {
+            self.deleteCollapse[key()] = ko.observable(false);
+        });
+    };
+
+    self.showWizard = () => {
         self.state('wizard');
     };
 
-    self.changeCharacter = function(character) {
+    self.changeCharacter = (character) => {
         // Don't switch to the same character.
         var activeCharacterKey = null;
         if (CharacterManager.activeCharacter()) {
@@ -44,13 +48,26 @@ export function CharacterPickerViewModel(params) {
         }
     };
 
-    self.removeCharacter = function(character) {
+    self.removeCharacter = (character) => {
         //Remove the character.
         character.delete();
         self.characters.remove(character);
+
+        self.deleteCollapse[character.key()](false);
     };
 
-    self.localStoragePercent = ko.computed(function() {
+    self.toggleDeleteWell = ({key}) => {
+        // Set the others to close.
+        self.characters().forEach(({key}) => {
+            self.deleteCollapse[key()](false);
+        });
+
+        // Open the one we need.
+        const value = !self.deleteCollapse[key()]();
+        self.deleteCollapse[key()](value);
+    };
+
+    self.localStoragePercent = ko.computed(() => {
         var n = self.characters().lenth; //Force ko to recompute on change.
         var used = JSON.stringify(localStorage).length / (0.5 * 1024 * 1024);
         return (used / self.totalLocalStorage * 100).toFixed(2);
