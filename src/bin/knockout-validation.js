981a6825a985a49b67d5f6f6cf6b612f20325df0
Brian Schrader <brian@brianschrader.com>
2018-08-12 16:22:40 -0700

    Reimplements validation. (#1814)
    ORIGINAL COMMIT MESSAGE:
    
    Adds a new validation framework and an example in Features. (#1769)
    * Adds a new validation framework and an example in Features.
    
    - Using the new validation, we can apply form validation to all modals
    and other places.
    - The Features has been updated to have an example of how to use this
    new validation including a new validation.js with the common rules for
    add/edit modals.
    - There is still work to be done in the handling of updates and
    submissions but this should get us close enough for now.
    
    * Removes debugging statement
    
    * Removes old data modal handler
    
    * Preserves the original onfocusout functionality.
    
    * Adds documentation and usage example
    
    * Fixes linting
    
    * Fixes linting II...the relintining
    
    * Fixes linting again..

---
+++ src/bin/knockout-validation.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import ko from 'knockout';
+import 'jquery-validation';
+
+/**
+* Binds a form element to use jQuery validation tools
+* The argument should be a an object of settings from jQuery Validate.
+*
+* jQuery Validation Docs:
+* https://jqueryvalidation.org/validate/
+*
+* Usage Example
+* -------------
+*  <form class="form-horizontal" data-bind="validate: validationSettings">
+*    <!-- Inputs in a validated form must have a name. -->
+*    <input name="name" value="" type="text" />
+*    <!-- Either buttons or input[type=submit] works fine.. -->
+*    <input type="submit" value="Add" />
+*  </form>
+*
+*/
+ko.bindingHandlers.validate = {
+    init: (element, valueAccessor) => {
+        const value = valueAccessor();
+
+        // Add a new handler callback that adds the form as a param for
+        // checking overall validity.
+        // Preserve the old onfocusout in case it was being used.
+        value.ogonfocusout = value.onfocusout;
+        value.onfocusout = (input, event) => {
+            if (value.updateHandler) {
+                value.updateHandler($(element), input);
+            }
+            if (value.ogonfocusout) {
+                value.ogonfocusout(input, event);
+            }
+        };
+
+        $(element).validate(ko.unwrap(value));
+    }
+};
