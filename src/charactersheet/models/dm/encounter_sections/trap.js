04055f8359e4b0035ce7ab64378d073226cc8086
Nathaniel Arellano <nathaniel.arellano@gmail.com>
2019-03-27 21:28:46 -0700

    Completes implementation of trap section

--- src/charactersheet/models/dm/encounter_sections/trap.js
+++ src/charactersheet/models/dm/encounter_sections/trap.js
@@ -15,6 +15,7 @@ export class Trap extends KOModel {
             'encounterUuid',
             'uuid',
             'name',
+            'level',
             'threat',
             'trigger',
             'effect',
@@ -33,6 +34,7 @@ export class Trap extends KOModel {
     uuid = ko.observable();
 
     name = ko.observable();
+    level = ko.observable();
     threat = ko.observable();
     trigger = ko.observable();
     effect = ko.observable();
@@ -59,12 +61,73 @@ export class Trap extends KOModel {
             this.SHORT_DESCRIPTION_MAX_LENGTH
         );
     });
+
+    longEffect = ko.pureComputed(() => {
+        return Utility.string.truncateStringAtLength(
+            this.effect(),
+            this.LONG_DESCRIPTION_MAX_LENGTH
+        );
+    });
+
+    shortEffect = ko.pureComputed(() => {
+        return Utility.string.truncateStringAtLength(
+            this.effect(),
+            this.SHORT_DESCRIPTION_MAX_LENGTH
+        );
+    });
+
+    longTrigger = ko.pureComputed(() => {
+        return Utility.string.truncateStringAtLength(
+            this.trigger(),
+            this.LONG_DESCRIPTION_MAX_LENGTH
+        );
+    });
+
+    shortTrigger = ko.pureComputed(() => {
+        return Utility.string.truncateStringAtLength(
+            this.trigger(),
+            this.SHORT_DESCRIPTION_MAX_LENGTH
+        );
+    });
+
+    nameLabel = ko.pureComputed(() => {
+        if (this) {
+            if (this.isActive()) {
+                return 'is armed';
+            } else {
+                return 'is not armed';
+            }
+        }
+    });
+
+    // determines if the "More Fields" div is collapsed or expanded in the edit form
+    // based on the presence of the "complex" trap fields
+    shouldShowMoreFields = ko.pureComputed(() => {
+        if (this) {
+            if (this.initiative()
+                || this.activeElements()
+                || this.dynamicElements()
+                || this.constantElements()) {
+                    return 'collapse in';
+            } else {
+                return 'collapse';
+            }
+        }
+        // default
+        return 'collapse';
+    });
 }
 
 Trap.validationConstraints = {
     rules: {
         name: {
             required: true,
+            maxlength: 256
+        },
+        level: {
+            maxlength: 256
+        },
+        threat: {
             maxlength: 256
         }
     }
