f461e892422bf3fe1580b531dd3e8aa3fb8283d8
David Mote <davidmote@gmail.com>
2019-05-06 23:21:46 -0700

    Updates other stats to use new scoped responsibilities approach

--- src/charactersheet/viewmodels/character/other_stats/form.js
+++ src/charactersheet/viewmodels/character/other_stats/form.js
@@ -1,232 +1,176 @@
-import 'bin/popover_bind';
-import {
-    AbilityScore,
-    OtherStats,
-    Profile
-} from 'charactersheet/models/character';
-import {
-    ArmorClassService,
-    ProficiencyService
-} from 'charactersheet/services';
 import {
     CoreManager,
-    Fixtures,
     Notifications
 } from 'charactersheet/utilities';
 
-import { FormComponentViewModel } from 'charactersheet/components';
-import { getModifier } from 'charactersheet/models/character/ability_score';
+import {
+    OtherStats,
+    Profile
+} from 'charactersheet/models/character';
+
 import ko from 'knockout';
 import template from './form.html';
 
 
-//extends FormComponentViewModel
-export class OtherStatsFormViewModel  {
+class ACFormViewModel {
     constructor(params) {
-        this.data = ko.utils.unwrapObservable(params.data);
-        // Actions
-        this.save = params.saveAction;
-        this.reset = params.cancelAction;
-
         // Card Properties
+        this.containerId = ko.utils.unwrapObservable(params.containerId);
         this.showForm = params.showForm;
         this.flip = params.flip;
         this.resize = params.resize;
 
-        this.add = params.add;
-        this.remove = params.remove;
-
-        this.containerId = ko.utils.unwrapObservable(params.containerId);
-
-        this.currentEditItem = ko.observable();
-
+        this.loaded = ko.observable(false);
         this.formElementHasFocus = ko.observable(false);
-
-        this.addForm = ko.observable(false);
-      // this.bypassUpdate = ko.observable(false);
-      // this.shouldShowDisclaimer = ko.observable(false);
     }
 
-    generateBlank = () => ({
-        profile: new Profile(),
-        otherStats: new OtherStats()
-    });
+    async load() {
+        this.loaded(false);
+        await this.refresh();
+        this.loaded(true);
+    }
 
-    notify = () => {
-        throw('you must provide a notification system');
+    async reset() {
+        await this.refresh();
+        this.flip();
+    }
+
+    async refresh() {
+        throw('refresh must be defined by subclasses of ACFormViewModel');
+    }
+
+    async save() {
+        throw('Save must be defined by subclasses of ACFormViewModel');
+    }
+
+    async notify() {
+        throw('Notify must be defined by subclasses of ACFormViewModel');
+    }
+
+    async submit() {
+        await this.save();
+        this.notify();
+        this.flip();
+    }
+
+    setUpSubscriptions() {
+        this.showForm.subscribe(this.subscribeToShowForm);
     }
 
     subscribeToShowForm = () => {
-      // if (this.showForm()) {
-      //     if (this.data) {
-      //         this.reset();
-      //         this.currentEditItem().importValues(this.data.exportValues());
-      //     }
-      //     this.formElementHasFocus(true);
-      // } else {
-      //     this.formElementHasFocus(false);
-      //     if (this.bypassUpdate()) {
-      //         this.bypassUpdate(false);
-      //     } else {
-      //         this.update();
-      //     }
-      //     this.reset();
-      // }
+        if (this.showForm()) {
+            this.refresh();
+            this.formElementHasFocus(true);
+        } else {
+            this.formElementHasFocus(false);
+        }
+    }
+}
+
+export class OtherStatsFormViewModel extends ACFormViewModel {
+    constructor(params) {
+        super(params);
+        this.otherStats = ko.observable(new OtherStats());
+        this.profile = ko.observable(new Profile());
+
+        // Notification Properties
+        this.hasProficiencyChanged = ko.observable(false);
+        this.hasInspirationChanged = ko.observable(false);
+        this.hasInitiativeChanged = ko.observable(false);
+        this.hasAcChanged = ko.observable(false);
+        this.hasLevelChanged = ko.observable(false);
+        this.hasExperienceChanged = ko.observable(false);
+
     }
 
-    load = () => {
-      // this.reset();
-      // if (this.data) {
-      //     this.currentEditItem().importValues(this.data.exportValues());
-      // } else {
-      //     this.addForm(true);
-      // }
-      //
-      // this.showForm.subscribe(this.subscribeToShowForm);
-      //
-      // this.shouldShowDisclaimer.subscribe(()=> {
-      //     setTimeout(this.resizeCallback, 1);
-      // });
+    validation = {
+        rules : {
+            // Deep copy of properties in object
+            ...Profile.validationConstraints.rules,
+            ...OtherStats.validationConstraints.rules
+        }
     }
-
-    update = () => {
-      // if (this.data) {
-      //     this.data.importValues(this.currentEditItem().exportValues());
-      //     this.data.save();
-      // } else {
-      //     this.addCallback(this.currentEditItem());
-      // }
-      // this.notify();
-    }
-
-    //save = () => {
-      // this.bypassUpdate(true);
-      // this.update();
-      // this.toggle();
-      // this.reset();
-    //}
-
-    cancel = (data, event) => {
-      // this.bypassUpdate(true);
-      // this.toggle();
-      // this.reset();
-    }
-
-    // reset = () => {
-      // this.shouldShowDisclaimer(false);
-      // this.currentEditItem(this.generateBlank());
-    // }
-
-    remove = () => {
-      // $(`#${this.containerId}`).collapse('hide');
-      // setTimeout(() => {
-      //     this.removeCallback(this.data);
-      //     this.notify();
-      // }, 650);
-    }
-
-
-    // constructor(params) {
-    //     super(params);
-    //     this.otherStats = ko.observable(new OtherStats());
-    //     this.profile = ko.observable(new Profile());
-    //     this.armorClass = ko.observable();
-    //     this.level = ko.observable('');
-    //     this.experience = ko.observable('');
-    //     this.proficiencyLabel = ko.observable();
-    //     this.initiativeLabel = ko.observable();
-    //     this.initiativePopover = ko.observable();
-    //     this.proficiencyPopover = ko.observable();
-    //     this.armorClassPopover = ko.observable();
-    //
-    //     this.hasProficiencyChanged = ko.observable(false);
-    //     this.hasInspirationChanged = ko.observable(false);
-    //     this.hasInitiativeChanged = ko.observable(false);
-    //     this.hasAcChanged = ko.observable(false);
-    //     this.hasLevelChanged = ko.observable(false);
-    //     this.hasExperienceChanged = ko.observable(false);
-    //
-    //     this.validation = {
-    //         rules : {
-    //             // Deep copy of properties in object
-    //             ...Profile.validationConstraints.rules,
-    //             ...OtherStats.validationConstraints.rules
-    //         }
-    //     };
-    // }
-
-    load = async () => {
-        // this.loaded(false);
-        // await this.reset();
-        //
-        // this.calculateInitiativeLabel();
-        // this.updateArmorClass();
-        // this.calculatedProficiencyLabel();
-        //
-        // // Subscriptions
-        // this.setUpModelSubscriptions();
-        // this.monitorSubscriptions();
-    }
-
-    monitorSubscriptions = () => {
-        // Notifications.armorClass.changed.add(this.updateArmorClass);
-        // Notifications.abilityScores.dexterity.changed.add(this.calculateInitiativeLabel);
-        // Notifications.proficiencyBonus.changed.add(this.calculatedProficiencyLabel);
-    }
-
-    setUpModelSubscriptions = () => {
-        // this.otherStats().proficiencyModifier.subscribe(this.proficiencyHasChanged);
-        // this.otherStats().inspiration.subscribe(this.inspirationHasChanged);
-        // this.otherStats().initiativeModifier.subscribe(this.initiativeHasChanged);
-        // this.otherStats().armorClassModifier.subscribe(this.armorClassModifierDataHasChanged);
-        // this.profile().level.subscribe(this.levelDataHasChanged);
-        // this.profile().experience.subscribe(this.experienceDataHasChanged);
-    }
-
-
-    // inspirationHasChanged = () => {
-    //     this.hasInspirationChanged(true);
-    // }
-    //
-    // initiativeHasChanged = () => {
-    //     this.hasInitiativeChanged(true);
-    // }
-    //
-    // armorClassModifierDataHasChanged = () => {
-    //     this.hasAcChanged(true);
-    // }
-    //
-    // levelDataHasChanged = () => {
-    //     this.hasLevelChanged(true);
-    // }
-    //
-    // experienceDataHasChanged = () => {
-    //     this.hasExperienceChanged(true);
-    // }
-    //
-    // proficiencyHasChanged = () => {
-    //     this.hasProficiencyChanged(true);
-    // }
-    //
-    // resetSubscriptions = () => {
-    //     this.setUpModelSubscriptions();
-    //     this.hasInspirationChanged(false);
-    //     this.hasAcChanged(false);
-    //     this.hasLevelChanged(false);
-    //     this.hasExperienceChanged(false);
-    //     this.hasProficiencyChanged(false);
-    //     this.hasInitiativeChanged(false);
-    // }
 
     toggleInspiration = async () => {
-        this.data.otherStats().inspiration(!this.data.otherStats().inspiration());
+        this.otherStats().inspiration(!this.otherStats().inspiration());
     }
 
-    submit = async () => {
-        await this.save();
-        if (this.flip) {
-            this.flip();
+    refresh = async () => {
+        const key = CoreManager.activeCore().uuid();
+        const otherStats = await OtherStats.ps.read({uuid: key});
+        const profile = await Profile.ps.read({uuid: key});
+        this.otherStats(otherStats.object);
+        this.profile(profile.object);
+        this.resetSubscriptions();
+    }
+
+    save = async () => {
+        // Do not call directly unless you know what you're doing. Call submit.
+        const otherStatsResponse = await this.otherStats().ps.save();
+        const profileResponse = await this.profile().ps.save();
+        this.otherStats(otherStatsResponse.object);
+        this.profile(profileResponse.object);
+    }
+
+    notify = async () => {
+        if (this.hasInspirationChanged()) {
+            Notifications.otherStats.inspiration.changed.dispatch();
         }
+        if (this.hasAcChanged()) {
+            Notifications.stats.armorClassModifier.changed.dispatch();
+        }
+        if (this.hasLevelChanged()) {
+            Notifications.profile.level.changed.dispatch();
+        }
+        if (this.hasExperienceChanged()) {
+            Notifications.profile.experience.changed.dispatch();
+        }
+        if (this.hasProficiencyChanged()) {
+            Notifications.otherStats.proficiency.changed.dispatch();
+        }
+        this.resetSubscriptions();
+    }
+
+    setUpSubscriptions = () => {
+        super.setUpSubscriptions();
+        // Subscribe to the fields changing so that the correct notifications
+        // are fired when saving.
+        this.otherStats().proficiencyModifier.subscribe(this.proficiencyHasChanged);
+        this.otherStats().inspiration.subscribe(this.inspirationHasChanged);
+        this.otherStats().initiativeModifier.subscribe(this.initiativeHasChanged);
+        this.otherStats().armorClassModifier.subscribe(this.armorClassModifierDataHasChanged);
+        this.profile().level.subscribe(this.levelDataHasChanged);
+        this.profile().experience.subscribe(this.experienceDataHasChanged);
+    }
+
+    // Functions to record that fields have changed. These values are reset on save (or reset)
+    inspirationHasChanged = () => {
+        this.hasInspirationChanged(true);
+    }
+    initiativeHasChanged = () => {
+        this.hasInitiativeChanged(true);
+    }
+    armorClassModifierDataHasChanged = () => {
+        this.hasAcChanged(true);
+    }
+    levelDataHasChanged = () => {
+        this.hasLevelChanged(true);
+    }
+    experienceDataHasChanged = () => {
+        this.hasExperienceChanged(true);
+    }
+    proficiencyHasChanged = () => {
+        this.hasProficiencyChanged(true);
+    }
+
+    resetSubscriptions = () => {
+        this.setUpSubscriptions();
+        this.hasInspirationChanged(false);
+        this.hasAcChanged(false);
+        this.hasLevelChanged(false);
+        this.hasExperienceChanged(false);
+        this.hasProficiencyChanged(false);
+        this.hasInitiativeChanged(false);
     }
 }
 
---
+++ src/charactersheet/viewmodels/character/other_stats/view.js
@@ -0,0 +1,162 @@
+import 'bin/popover_bind';
+import {
+    AbilityScore,
+    OtherStats,
+    Profile
+} from 'charactersheet/models/character';
+
+import {
+    ArmorClassService,
+    ProficiencyService
+} from 'charactersheet/services';
+
+import {
+    CoreManager,
+    Fixtures,
+    Notifications
+} from 'charactersheet/utilities';
+
+import { getModifier } from 'charactersheet/models/character/ability_score';
+
+import ko from 'knockout';
+import template from './view.html';
+
+class ACViewModel {
+    constructor(params) {
+        // Card Properties
+        this.containerId = ko.utils.unwrapObservable(params.containerId);
+        this.showForm = params.showForm;
+        this.flip = params.flip;
+        this.resize = params.resize;
+
+        this.loaded = ko.observable(false);
+    }
+
+    async load() {
+        this.loaded(false);
+        await this.refresh();
+        this.setUpSubscriptions();
+        this.loaded(true);
+    }
+
+    async refresh() {
+        throw('refresh must be defined by subclasses of ACViewModel');
+    }
+
+    setUpSubscriptions() {
+        this.showForm.subscribe(this.subscribeToShowForm);
+    }
+
+    subscribeToShowForm = () => {
+        if (!this.showForm()) {
+            this.refresh();
+        }
+    }
+}
+
+export class OtherStatsViewModel extends ACViewModel {
+    constructor(params) {
+        super(params);
+
+        this.otherStats = ko.observable(new OtherStats());
+        this.profile = ko.observable(new Profile());
+
+        // Calculated Field
+        this.armorClass = ko.observable();
+
+        this.proficiencyLabel = ko.observable();
+        this.initiativeLabel = ko.observable();
+
+        this.initiativePopover = ko.observable();
+        this.proficiencyPopover = ko.observable();
+        this.armorClassPopover = ko.observable();
+    }
+
+    async load() {
+        await super.load();
+        this.calculateInitiativeLabel();
+        this.updateArmorClass();
+        this.calculatedProficiencyLabel();
+        // Subscriptions
+        this.monitorSubscriptions();
+    }
+
+    refresh = async () => {
+        const key = CoreManager.activeCore().uuid();
+        const otherStats = await OtherStats.ps.read({uuid: key});
+        const profile = await Profile.ps.read({uuid: key});
+        this.otherStats(otherStats.object);
+        this.profile(profile.object);
+    }
+
+    monitorSubscriptions = () => {
+        Notifications.armorClass.changed.add(this.updateArmorClass);
+        Notifications.abilityScores.dexterity.changed.add(this.calculateInitiativeLabel);
+        Notifications.proficiencyBonus.changed.add(this.calculatedProficiencyLabel);
+    }
+
+    // Calculate proficiency label and popover
+    calculatedProficiencyLabel = async () => {
+        const proficiencyService = ProficiencyService.sharedService();
+        var level = await proficiencyService.proficiencyBonusByLevel();
+        const proficiency = parseInt(this.otherStats().proficiencyModifier());
+        this.updateProficiencyPopoverMessage(level, proficiency);
+        this.proficiencyLabel(proficiencyService.proficiency());
+    }
+
+    updateProficiencyPopoverMessage = (level, proficiency) => {
+        this.proficiencyPopover('<span style="white-space:nowrap;"><strong>Proficiency</strong> = '
+            + '(<strong>Level</strong> / 4) + 1 + <strong>Modifier</strong></span><br />'
+            + 'Proficiency = ${level} + 1 + ${proficiency}');
+    }
+
+    // Calculate initiative label and popover
+    calculateInitiativeLabel = async () => {
+        const key = CoreManager.activeCore().uuid();
+        const response = await AbilityScore.ps.list({
+            coreUuid: key,
+            name: Fixtures.abilityScores.constants.dexterity.name
+        });
+        const dexterityModifier = response.objects[0].getModifier();
+        const initiativeModifier = this.otherStats().initiativeModifier();
+
+        this.updateInitiativePopoverMessage(dexterityModifier, initiativeModifier);
+        this.initiativeLabel(parseInt(dexterityModifier) + parseInt(initiativeModifier));
+    }
+
+    updateInitiativePopoverMessage = (dexterityModifier, initiativeModifier) => {
+        this.initiativePopover('<span style="white-space:nowrap;"><strong>Initiative</strong> = ' +
+        'Dexterity Modifier + Modifier</span><br />' +
+        'Initiative = ${dexterityModifier} + ${initiativeModifier}');
+    }
+
+    updateArmorClassPopoverMessage = async () => {
+        const acService = ArmorClassService.sharedService();
+        const baseAC = acService.baseArmorClass();
+        const dexMod = await acService.dexBonusFromArmor();
+        const magicModifiers = acService.equippedArmorMagicalModifier() + acService.equippedShieldMagicalModifier();
+        const shield = acService.getEquippedShieldBonus();
+
+        const modifier = this.otherStats().armorClassModifier() ? this.otherStats().armorClassModifier() : 0;
+
+        this.armorClassPopover('<span><strong>Armor Class</strong> = '
+         + 'Base AC + Dexterity Modifier + Magical Modifier(s) + Shield + Modifier</span><br />'
+         + '<strong>Armor Class</strong> = ${baseAC} + ${dexMod} + ${magicModifiers} + ${shield} + ${modifier}');
+    }
+
+    updateArmorClass = async () => {
+        await this.updateArmorClassPopoverMessage();
+        this.armorClass(ArmorClassService.sharedService().armorClass());
+    }
+
+    toggleInspiration = async () => {
+        this.otherStats().inspiration(!this.otherStats().inspiration());
+        await this.otherStats().ps.save();
+        Notifications.otherStats.inspiration.changed.dispatch();
+    }
+}
+
+ko.components.register('other-stats-view', {
+    viewModel: OtherStatsViewModel,
+    template: template
+});
