25dae2f0fe08692418d4500a110e131ca5d6cadf
David Mote <davidmote@gmail.com>
2019-06-23 22:39:12 -0700

    match spell colors to tracked colors

--- src/charactersheet/viewmodels/character/spell_slots/index.js
+++ src/charactersheet/viewmodels/character/spell_slots/index.js
@@ -76,29 +76,7 @@ class SpellSlotsViewModel extends AbstractTabularViewModel {
     };
 
     mapToColor = (level) => {
-        switch (level.toString()) {
-        case '1':
-            return '#e74c3c'; //'#d9534f'; // red
-        case '2':
-            return '#e67e22'; //'#f0ad4e'; // orange
-        case '3':
-            return '#f1c40f'; //'#D7DF01'; // yellow
-        case '4':
-            return '#1abc9c'; //'#2F972F'; // forest
-        case '5':
-            return '#2ecc71'; //'#01DFD7'; // teal
-        case '6':
-            return '#3498db'; //'#71D4E8'; // sky blue
-        case '7':
-            return '#9b59b6'; //'#8000FF'; // indigo
-        case '8':
-            return '#34495e'; //'#800080'; // purple
-        case '9':
-            return '#95a5a6'; //'#906713'; //brown
-
-        default:
-            return '#777';
-        }
+        return Fixtures.general.colorHexList[level % 12];
     };
 
     mapToChart(slot) {
