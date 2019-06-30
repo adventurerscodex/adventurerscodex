c1553484a0e308e9ed04a4475d6c241ea61c072f
David Mote <davidmote@gmail.com>
2019-06-30 13:50:44 -0700

    Fixes imports and keys.js to fix unit tests

--- src/charactersheet/models/keys.js
+++ src/charactersheet/models/keys.js
@@ -1,14 +1,14 @@
 import * as characterClazz from './character';
 import * as commonClazz from './common';
 
-import { forIn, set } from 'lodash';
+import { forIn, get, set } from 'lodash';
 
 /* Generate a key/value set to generate new classes from its name */
 
 const allClazz = {};
 
 const mapClazz = (clazz) => {
-    if (clazz.name) {
+    if (get(clazz, 'name', false)) {
         set(allClazz, clazz.name, clazz );
     }
 };
