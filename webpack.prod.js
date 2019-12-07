4582d64ea78f6bd337c44ca359203e4ec725c3f6
Brian Schrader <brian@brianschrader.com>
2019-12-06 18:13:55 -0800

    Hotfix/babel issue (#1996)
    * Hotfixes an issue caused by babel uglification.
    
    This fix is not suitable for production. It is mearly here to get
    nightly up and running again.
    
    * Fixes mangling issues
    
    * Removes previous hack

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,19 +2,27 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common.js');
 const package_ = require('./package.json');
 
 let config = merge(common, {
     mode: 'production',
-    // HACKALERT: THIS IS NOT SUITABLE FOR PRODUCTION. DEV SOURCEMAPS HIDE ERRORS
-    // AND THIS NEEDS TO BE ADDRESSED BEFORE SHIPPING.
-    devtool: 'cheap-module-eval-source-map',
     output: {
         filename: '[name].[chunkhash].js'
     },
     optimization: {
         minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+                terserOptions: {
+                    mangle: false,
+                }
+            })
+        ],
         splitChunks: {
             chunks: 'all'
         }
