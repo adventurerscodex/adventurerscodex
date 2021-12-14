94f768ff9a0431e05fb70f7f78faa8d98a5ac5d5
Brian Schrader <brian@brianschrader.com>
2021-12-13 22:42:14 -0800

    Refactors Coins

--- src/charactersheet/models/dm/encounter_sections/encounter_coins.js
+++ src/charactersheet/models/dm/encounter_sections/encounter_coins.js
@@ -25,7 +25,7 @@ export class EncounterCoins extends KOModel {
     silver = ko.observable(0);
     copper = ko.observable(0);
 
-    nameLabel = ko.pureComputed(() => {
+    name = ko.pureComputed(() => {
         return 'Coins';
     });
 
@@ -33,7 +33,7 @@ export class EncounterCoins extends KOModel {
         return 'N/A';
     });
 
-    descriptionLabel = ko.pureComputed(() => {
+    shortDescription = ko.pureComputed(() => {
         return this.worthInGold() ? this.worthInGold() + '(gp)' : '';
     });
 
@@ -83,45 +83,45 @@ export class EncounterCoins extends KOModel {
         treasure.value = pick(params, EncounterCoins.wealthFields);
         return treasure;
     };
-
-    buildModelFromValues = (values) => {
-        let keys = Object.keys(values);
-        keys.forEach((key) => {
-            this[key] = values[key];
-        });
-    };
-
-    getValues = () => {
-        let values = {};
-        EncounterCoins.wealthFields.forEach((field) => {
-            values[field] = this[field];
-        });
-
-        return values;
-    };
-
-    /**
-      * Serialize the current item to a plain JSON format. We use these in-leiu of the normal
-      * import/exportValues because those return a format unsuitable for re-importing
-      * (since it caused data corruption).
-     */
-    toJSON = () => {
-        let values = {};
-        EncounterCoins.allFields.forEach((field) => {
-            values[field] = this[field]();
-        });
-
-        return values;
-    };
-
-    /**
-      * De-serialize the current item into the current model. We use these in-leiu of the normal
-      * import/exportValues because those return a format unsuitable for re-importing
-      * (since it caused data corruption).
-     */
-    fromJSON = (values) => {
-        EncounterCoins.allFields.forEach((field) => {
-            this[field](values[field]);
-        });
-    }
 }
+
+
+EncounterCoins.validationConstraints = {
+    fieldParams: {
+        platinum: {
+            required: true,
+            type: 'number',
+            max: 10000,
+            min: -10000,
+            step: 1,
+        },
+        gold: {
+            required: true,
+            type: 'number',
+            max: 10000,
+            min: -10000,
+            step: 1,
+        },
+        electrum: {
+            required: true,
+            type: 'number',
+            max: 10000,
+            min: -10000,
+            step: 1,
+        },
+        silver: {
+            required: true,
+            type: 'number',
+            max: 10000,
+            min: -10000,
+            step: 1,
+        },
+        copper: {
+            required: true,
+            type: 'number',
+            max: 10000,
+            min: -10000,
+            step: 1,
+        },
+    },
+};
--- src/charactersheet/viewmodels/dm/encounter_sections/treasure_section/components/coins.form.js
+++ src/charactersheet/viewmodels/dm/encounter_sections/treasure_section/components/coins.form.js
@@ -1,6 +1,48 @@
+import autoBind from 'auto-bind';
+import { Fixtures } from 'charactersheet/utilities';
+import { AbstractTreasureFormViewModel } from 'charactersheet/viewmodels/abstract';
+import { EncounterCoins, Treasure } from 'charactersheet/models/dm';
 import ko from 'knockout';
 import template from './coins.form.html';
+import { SELECTDATA } from 'charactersheet/constants';
+
+import cpCoin from 'images/cp-coin.svg';
+import epCoin from 'images/ep-coin.svg';
+import gpCoin from 'images/gp-coin.svg';
+import spCoin from 'images/sp-coin.svg';
+import ppCoin from 'images/pp-coin.svg';
+
+
+class EncounterCoinsFormViewModel extends AbstractTreasureFormViewModel {
+
+    constructor(params) {
+        super(params);
+        autoBind(this);
+    }
+
+    modelClass() {
+        return Treasure;
+    }
+
+    treasureClass() {
+        return EncounterCoins;
+    }
+
+    treasureType() {
+        return Treasure.TYPE.COINS;
+    }
+
+    icons = {
+        cpCoin,
+        spCoin,
+        epCoin,
+        gpCoin,
+        ppCoin,
+    };
+}
+
 
 ko.components.register('treasure-coins-form', {
+    viewModel: EncounterCoinsFormViewModel,
     template: template
 });
