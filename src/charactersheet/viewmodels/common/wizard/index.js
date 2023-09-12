17f9017432787636d2f964c72cbe72e41166da9a
Brian Schrader <brian@brianschrader.com>
2023-09-12 16:28:12 -0700

    Fixes bug with pre-pop since Features apparently have a different format than the other data repository types.

--- src/charactersheet/viewmodels/common/wizard/index.js
+++ src/charactersheet/viewmodels/common/wizard/index.js
@@ -108,11 +108,11 @@ export class WizardViewModel {
             DataRepository.traits[key].race.toLowerCase() === this.race().toLowerCase()
         )).map((key) => ({ ...DataRepository.traits[key], tracked: null }));
 
-        const features = Object.keys(DataRepository.features).filter(key => (
-            DataRepository.features[key].characterClass.toLowerCase() === this.characterClass().toLowerCase()
-        )).filter(key => (
-            parseInt(DataRepository.features[key].level) <= parseInt(this.level())
-        )).map((key) => ({ ...DataRepository.features[key], tracked: null }));
+        const features = DataRepository.features.filter(feature => (
+            feature.characterClass.toLowerCase() === this.characterClass().toLowerCase()
+        )).filter(feature => (
+            parseInt(feature.level) <= parseInt(this.level())
+        )).map(feature => ({ ...feature, tracked: null }));
 
         const items = Object.keys(DataRepository.backpacks).filter(key => (
             key.toLowerCase() === this.backpack().toLowerCase()
