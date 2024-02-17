3e9ecce68ee2e4899d843fe843b1d97943316f94
Brian Schrader <brian@brianschrader.com>
2024-02-16 20:36:19 -0800

    Deleting a note now requires confirmation through a browser confrmation dialog box (#2164)

--- src/charactersheet/viewmodels/common/notes/form.js
+++ src/charactersheet/viewmodels/common/notes/form.js
@@ -5,6 +5,13 @@ import autoBind from 'auto-bind';
 import ko from 'knockout';
 import template from './form.html';
 
+const DELETE_CONFIRM_MESSAGE = `You are About to Delete a Note.
+
+
+This will permanently delete all data associated with this note. This action cannot be undone.
+
+Click Ok to delete this Note.`;
+
 export class NotesFormViewModel extends AbstractChildFormModel {
     constructor(params) {
         super(params);
@@ -20,6 +27,12 @@ export class NotesFormViewModel extends AbstractChildFormModel {
             this.entity().updateTitleFromHeadline();
         }
         await super.save();
+    }
+
+    async confirmDelete() {
+        if (confirm(DELETE_CONFIRM_MESSAGE)) {
+            await this.delete();
+        }
     }
 
     validation = {
