8b20beb49630ce4d00b971eff3c2597c56e9e775
Brian Schrader <brian@brianschrader.com>
2024-03-20 16:11:31 -0700

    Fixes GPT Elaborations for DMs after refactor for players changed the schema. (#2199)

--- src/charactersheet/models/common/elaboration.js
+++ src/charactersheet/models/common/elaboration.js
@@ -3,7 +3,7 @@ import ko from 'knockout';
 
 
 export class Elaboration extends KOModel {
-    static __skeys__ = ['elaborate'];
+    static __skeys__ = ['elaborate', 'campaigns', 'encounters'];
 
     static mapping = {
         include: []
