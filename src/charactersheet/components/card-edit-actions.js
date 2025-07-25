351c2e83b238d7623276e59aa0498d004cf6d36e
Brian Schrader <brian@brianschrader.com>
2025-07-25 11:54:51 -0700

    Adds Share Sheet Settings (#2215)
    * Allows DMs to make share links to Campaign sharesheets
    
    * Adds Share Sheet Settings and Rewrites Old Share Component JS
    
    * Only Patrons can customize share settings
    
    * Fixes copy/paste
    
    * Adds disclaimer to share sheet settings
    
    * Trial of view mode for DMS
    
    * Fixes copying to clipboard and renames param for sharekeys

--- src/charactersheet/components/card-edit-actions.js
+++ src/charactersheet/components/card-edit-actions.js
@@ -1,5 +1,14 @@
 import ko from 'knockout';
 
+const ViewMode = {
+    // Show all fields in the UI
+    all: 'all',
+    // Show the UI as it would appear in the exhibit
+    exhibit: 'exhibit',
+    // Show the UI as it would appear in the player share-sheet.
+    player: 'player',
+};
+
 /**
  * card-edit-actions component
  *
@@ -14,18 +23,51 @@ import ko from 'knockout';
  *  }"></card-edit-actions>
  */
 export class CardEditActionComponent {
+
+    // Show the `next` value for the text.
+    modes = {
+        [ViewMode.all]: 'View as Exhibit',
+        [ViewMode.exhibit]: 'View as Player Mode',
+        [ViewMode.player]: 'View All',
+    };
+
     constructor(params) {
         this.flip = params.flip;
+        this.mode = params.mode;
     }
+
+    text = ko.pureComputed(() => this.modes[this.mode()]);
+
     clickFlip = () => {
         this.flip();
     }
+
+    clickToggleDisplayMode = () => {
+        if (!this.mode) { return; }
+
+        if (this.mode() === ViewMode.all) {
+            this.mode(ViewMode.exhibit);
+        } else if (this.mode() === ViewMode.exhibit) {
+            this.mode(ViewMode.player)
+        } else {
+            this.mode(ViewMode.all);
+        }
+    }
+
 }
 
 ko.components.register('card-edit-actions', {
     viewModel: CardEditActionComponent,
     template: '\
     <div class="btn-group edit-btn" role="group" aria-label="actionButtons">\
+      <!-- ko if: mode -->\
+      <button class="btn btn-link text-hover-only" data-bind="click: $component.clickToggleDisplayMode">\
+        <!-- ko if: text -->\
+        <span class="mr-1 text-muted" data-bind="text: text"></span>\
+        <!-- /ko -->\
+        <i class="fa fa-eye fa-color-hover-success fa-color fa-lg clickable" aria-hidden="true"></i>\
+      </button>\
+      <!-- /ko -->\
       <button class="btn btn-link" data-bind="click: $component.clickFlip">\
         <span class="fa fa-pencil-square-o fa-color-hover-success fa-color fa-lg clickable" aria-hidden="true"></span>\
       </button>\
---
+++ src/charactersheet/viewmodels/common/share/form.js
@@ -0,0 +1,25 @@
+import { AbstractChildFormModel } from 'charactersheet/viewmodels/abstract';
+import { ShareKey } from 'charactersheet/models/common';
+
+import autoBind from 'auto-bind';
+import ko from 'knockout';
+import template from './form.html';
+
+export class ShareKeyFormViewModel extends AbstractChildFormModel {
+    constructor(params) {
+        super(params);
+        autoBind(this);
+
+        this.core = params.core;
+        this.userIsPatron = params.userIsPatron;
+    }
+
+    modelClass () {
+        return ShareKey;
+    }
+}
+
+ko.components.register('sharekey-form', {
+    viewModel: ShareKeyFormViewModel,
+    template: template
+});
